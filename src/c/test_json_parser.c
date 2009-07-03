8e9674a1b88b8c84d558534189dc10acea84bb15
Douglass Cutting <cutting@apache.org>
2009-07-03 04:34:31 +0000

    AVRO-68.  Add license headers to C sources and improve C packaging.  Contributed by Matt Massie.
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/avro/trunk@790785 13f79535-47bb-0310-9956-ffa450edef68

--- src/c/test_json_parser.c
+++ src/c/test_json_parser.c
@@ -1,3 +1,22 @@
+/*
+Licensed to the Apache Software Foundation (ASF) under one
+or more contributor license agreements.  See the NOTICE file
+distributed with this work for additional information
+regarding copyright ownership.  The ASF licenses this file
+to you under the Apache License, Version 2.0 (the
+"License"); you may not use this file except in compliance
+with the License.  You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing,
+software distributed under the License is distributed on an
+"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+KIND, either express or implied.  See the License for the
+specific language governing permissions and limitations
+under the License.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -28,7 +47,7 @@ static struct test_dir
 FILE *trace = NULL;
 
 int
-main (int argc, char *argv[])
+main (int argc, char *argv[], char *envp[])
 {
   FILE *file;
   DIR *dir;
@@ -37,6 +56,12 @@ main (int argc, char *argv[])
   char buf[1024];
   apr_pool_t *pool;
   JSON_value *value;
+  char path[256];
+  char *srcdir = getenv ("srcdir");
+  if (!srcdir)
+    {
+      srcdir = ".";
+    }
 
   apr_initialize ();
   atexit (apr_terminate);
@@ -54,10 +79,11 @@ main (int argc, char *argv[])
   for (i = 0; i < NUM_TEST_DIRS; i++)
     {
       struct test_dir *td = test_dirs + i;
-      dir = opendir (td->path);
+      snprintf (path, sizeof (path), "%s/%s", srcdir, td->path);
+      dir = opendir (path);
       if (dir == NULL)
 	{
-	  fprintf (stderr, "Unable to open '%s'\n", td->path);
+	  fprintf (stderr, "Unable to open '%s'\n", path);
 	  return EXIT_FAILURE;
 	}
       dent = readdir (dir);
@@ -66,7 +92,7 @@ main (int argc, char *argv[])
 	  if (dent->d_name[0] != '.')
 	    {
 	      char filepath[256];
-	      snprintf (filepath, sizeof (filepath), "%s/%s", td->path,
+	      snprintf (filepath, sizeof (filepath), "%s/%s", path,
 			dent->d_name);
 
 	      file = fopen (filepath, "r");
