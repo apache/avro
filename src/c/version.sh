5f7cb7d6d00a284364c629ba85d4b3114c677cc2
Matthew Lee Massie <massie@apache.org>
2009-10-23 18:57:24 +0000

    AVRO-168. Correct shared library versioning for C implementation
    git-svn-id: https://svn.apache.org/repos/asf/hadoop/avro/trunk@829178 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/c/version.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#
+# This script is used to generate version numbers for autotools
+#
+# The top-level main version is collected from the top-level build.xml
+#
+# The information for libtool is maintained manually since
+# the public API for the C library can change independent of the project
+#
+# Do each of these steps in order and libtool will do the right thing
+# (1) If there are changes to libavro:
+#         libavro_micro_version++
+#         libavro_interface_age++ 
+#         libavro_binary_age++
+# (2) If any functions have been added:
+#         libavro_interface_age = 0
+# (3) If backwards compatibility has been broken:
+#         libavro_binary_age = 0
+#         libavro_interface_age = 0
+#
+libavro_micro_version=1
+libavro_interface_age=1
+libavro_binary_age=1
+
+# IGNORE EVERYTHING ELSE FROM HERE DOWN.........
+if test $# != 1; then
+	echo "USAGE: $0 CMD"
+  	echo "  where CMD is one of: project, libtool, libcurrent, librevision, libage"
+	exit 1
+fi
+
+# http://sources.redhat.com/autobook/autobook/autobook_91.html
+# 'Current' is the most recent interface number that this library implements
+libcurrent=$(($libavro_micro_version - $libavro_interface_age))
+# The implementation number of the 'current' interface
+librevision=$libavro_interface_age
+# The difference between the newest and oldest interfaces that this library implements
+# In other words, the library implements all the interface numbers in the range from 
+# number 'current - age' to current
+libage=$(($libavro_binary_age - $libavro_interface_age))
+
+if test "$1" = "project"; then
+	build_xml="../../build.xml"
+	project_ver="undef"
+	if test -f $build_xml; then
+		project_ver=$(sed -n '/name="version"/s/.*value="\(.*\)".*$/\1/p' $build_xml)
+	fi
+	printf "%s" $project_ver
+elif test "$1" = "libtool"; then
+	# useful for the -version-info flag for libtool
+	printf "%d:%d:%d" $libcurrent $librevision $libage
+elif test "$1" = "libcurrent"; then
+	printf "%d" $libcurrent
+elif test "$1" = "librevision"; then
+	printf "%d" $librevision
+elif test "$1" = "libage"; then
+	printf "%d" $libage
+fi
