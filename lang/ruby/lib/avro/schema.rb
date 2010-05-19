abdbc5744d15001bf473cf0701ad3b1b490e6fde
Jeff Hodges <jmhodges@apache.org>
2010-05-19 22:12:30 +0000

    AVRO-543. Schema comparison is hella slow on the Ruby side.
    git-svn-id: https://svn.apache.org/repos/asf/avro/branches/branch-1.3@946457 13f79535-47bb-0310-9956-ffa450edef68

--- lang/ruby/lib/avro/schema.rb
+++ lang/ruby/lib/avro/schema.rb
@@ -129,12 +129,12 @@ module Avro
       @type.hash
     end
 
-    def to_hash
+    def to_avro
       {'type' => @type}
     end
 
     def to_s
-      Yajl.dump to_hash
+      Yajl.dump to_avro
     end
 
     class NamedSchema < Schema
@@ -145,7 +145,7 @@ module Avro
         names = Name.add_name(names, self)
       end
 
-      def to_hash
+      def to_avro
         props = {'name' => @name}
         props.merge!('namespace' => @namespace) if @namespace
         super.merge props
@@ -194,8 +194,8 @@ module Avro
         fields.inject({}){|hsh, field| hsh[field.name] = field; hsh }
       end
 
-      def to_hash
-        hsh = super.merge('fields' => @fields.map {|f|Yajl.load(f.to_s)} )
+      def to_avro
+        hsh = super.merge('fields' => @fields.map {|f| f.to_avro } )
         if type == 'request'
           hsh['fields']
         else
@@ -224,11 +224,11 @@ module Avro
         end
       end
 
-      def to_hash
+      def to_avro
         name_or_json = if items_schema_from_names
                          items.fullname
                        else
-                         Yajl.load(items.to_s)
+                         items.to_avro
                        end
         super.merge('items' => name_or_json)
       end
@@ -253,12 +253,12 @@ module Avro
         @values = values_schema
       end
 
-      def to_hash
+      def to_avro
         to_dump = super
         if values_schema_from_names
           to_dump['values'] = values
         else
-          to_dump['values'] = Yajl.load(values.to_s)
+          to_dump['values'] = values.to_avro
         end
         to_dump
       end
@@ -299,7 +299,7 @@ module Avro
         end
       end
 
-      def to_s
+      def to_avro
         # FIXME(jmhodges) this from_name pattern is really weird and
         # seems code-smelly.
         to_dump = []
@@ -307,10 +307,10 @@ module Avro
           if schema_from_names_indices.include?(i)
             to_dump << schema.fullname
           else
-            to_dump << Yajl.load(schema.to_s)
+            to_dump << schema.to_avro
           end
         end
-        Yajl.dump(to_dump)
+        to_dump
       end
     end
 
@@ -325,7 +325,7 @@ module Avro
         @symbols = symbols
       end
 
-      def to_hash
+      def to_avro
         super.merge('symbols' => symbols)
       end
     end
@@ -340,8 +340,9 @@ module Avro
         super(type)
       end
 
-      def to_s
-        to_hash.size == 1 ? type.inspect : Yajl.dump(to_hash)
+      def to_avro
+        hsh = super
+        hsh.size == 1 ? type : hsh
       end
     end
 
@@ -356,7 +357,7 @@ module Avro
         @size = size
       end
 
-      def to_hash
+      def to_avro
         super.merge('size' => @size)
       end
     end
@@ -377,8 +378,8 @@ module Avro
         @order = order
       end
 
-      def to_hash
-        sigh_type = type_from_names ? type.fullname : Yajl.load(type.to_s)
+      def to_avro
+        sigh_type = type_from_names ? type.fullname : type.to_avro
         hsh = {
           'name' => name,
           'type' => sigh_type
@@ -386,10 +387,6 @@ module Avro
         hsh['default'] = default if default
         hsh['order'] = order if order
         hsh
-      end
-
-      def to_s
-        Yajl.dump(to_hash)
       end
     end
   end
