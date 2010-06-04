79fb7bd60fac7e8ec35b2d44291266071445c44c
Jeff Hodges <jmhodges@apache.org>
2010-06-04 05:25:53 +0000

    AVRO-486. DataFile.open for the ruby side. merge from trunk
    git-svn-id: https://svn.apache.org/repos/asf/avro/branches/branch-1.3@951269 13f79535-47bb-0310-9956-ffa450edef68

--- lang/ruby/test/test_datafile.rb
+++ lang/ruby/test/test_datafile.rb
@@ -14,7 +14,7 @@ class TestDataFile < Test::Unit::TestCase
     end
   end
 
-  def test_differing_schemas
+  def test_differing_schemas_with_primitives
     writer_schema = <<-JSON
 { "type": "record",
   "name": "User",
@@ -28,15 +28,9 @@ JSON
     data = [{"username" => "john", "age" => 25, "verified" => true},
             {"username" => "ryan", "age" => 23, "verified" => false}]
 
-    file = File.open('data.avr', 'wb')
-    schema = Avro::Schema.parse(writer_schema)
-    writer = Avro::IO::DatumWriter.new(schema)
-    dw = Avro::DataFile::Writer.new(file, writer, schema)
-    data.each{|h| dw << h }
-    dw.close
-
-    file = File.open('data.avr', 'r+')
-    dr = Avro::DataFile::Reader.new(file, Avro::IO::DatumReader.new)
+    Avro::DataFile.open('data.avr', 'w', writer_schema) do |dw|
+      data.each{|h| dw << h }
+    end
 
     # extract the username only from the avro serialized file
     reader_schema = <<-JSON
@@ -47,13 +41,11 @@ JSON
  ]}
 JSON
 
-    reader = Avro::IO::DatumReader.new(nil, Avro::Schema.parse(reader_schema))
-    dr = Avro::DataFile::Reader.new(file, reader)
-    run = false
-    dr.each_with_index do |record, i|
-      run = true
-      assert_equal data[i]['username'], record['username']
+    Avro::DataFile.open('data.avr', 'r', reader_schema) do |dr|
+      dr.each_with_index do |record, i|
+        assert_equal data[i]['username'], record['username']
+      end
     end
-    assert run, "enumerable is run through at least once"
   end
+
 end
