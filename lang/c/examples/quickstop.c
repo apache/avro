0ad579a6276a75f018d60be364534ad2d142db69
Douglas Adam Creager <dcreager@apache.org>
2012-03-01 03:54:53 +0000

    AVRO-1039. C: Don't try to use nonexistent codecs in test cases.
    The quickstop example was hard-coded to use the deflate codec, even if
    we're not building that codec.  The null codec will always be there, so
    we fall back on that if we don't have deflate.
    
    
    git-svn-id: https://svn.apache.org/repos/asf/avro/trunk@1295401 13f79535-47bb-0310-9956-ffa450edef68

--- lang/c/examples/quickstop.c
+++ lang/c/examples/quickstop.c
@@ -21,6 +21,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#ifdef DEFLATE_CODEC
+#define QUICKSTOP_CODEC  "deflate"
+#else
+#define QUICKSTOP_CODEC  "null"
+#endif
+
 avro_schema_t person_schema;
 int64_t id = 0;
 
@@ -140,7 +146,8 @@ int main(void)
 	/* Delete the database if it exists */
 	unlink(dbname);
 	/* Create a new database */
-	rval = avro_file_writer_create_with_codec(dbname, person_schema, &db, "deflate", 0);
+	rval = avro_file_writer_create_with_codec
+	    (dbname, person_schema, &db, QUICKSTOP_CODEC, 0);
 	if (rval) {
 		fprintf(stderr, "There was an error creating %s\n", dbname);
 		fprintf(stderr, " error message: %s\n", avro_strerror());
