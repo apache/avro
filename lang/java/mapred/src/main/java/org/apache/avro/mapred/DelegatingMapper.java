49593cc38c388b89b85433b4533501a2d11e722f
Doug Cutting <cutting@apache.org>
2014-02-04 23:34:02 +0000

    AVRO-1439. Java: Add AvroMultipleInputs for mapred.  Contributed by Harsh J.
    git-svn-id: https://svn.apache.org/repos/asf/avro/trunk@1564562 13f79535-47bb-0310-9956-ffa450edef68

--- lang/java/mapred/src/main/java/org/apache/avro/mapred/DelegatingMapper.java
+++ lang/java/mapred/src/main/java/org/apache/avro/mapred/DelegatingMapper.java
@@ -28,37 +28,39 @@ import org.apache.hadoop.mapred.OutputCollector;
 import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.util.ReflectionUtils;
 
-/** Bridge between a {@link org.apache.hadoop.mapred.Mapper} and an {@link
- * AvroMapper}.  Outputs are written directly when a job is map-only, but are
- * otherwise assumed to be pairs that are split. */
-class HadoopMapper<IN,OUT,K,V,KO,VO> extends MapReduceBase
-  implements Mapper<AvroWrapper<IN>, NullWritable, KO, VO> {
-    
-  private AvroMapper<IN,OUT> mapper;
-  private MapCollector<OUT,K,V,KO,VO> out;
-  private boolean isMapOnly;
+/**
+ * An {@link Mapper} that delegates behaviour of paths to multiple other
+ * mappers. Similar to {@link HadoopMapper}, but instantiates map classes
+ * in the map() call instead of during configure(), as we rely on the split
+ * object to provide us that information.
+ *
+ * @see {@link AvroMultipleInputs#addInputPath(JobConf, Path, Class, Schema)}
+ */
+class DelegatingMapper<IN,OUT,K,V,KO,VO> extends MapReduceBase
+implements Mapper<AvroWrapper<IN>,NullWritable,KO,VO>
+{
+  AvroMapper<IN, OUT> mapper;
+  JobConf conf;
+  boolean isMapOnly;
+  AvroCollector<OUT> out;
 
-  @Override @SuppressWarnings("unchecked")
   public void configure(JobConf conf) {
-    this.mapper =
-      ReflectionUtils.newInstance
-      (conf.getClass(AvroJob.MAPPER, AvroMapper.class, AvroMapper.class),
-       conf);
+    this.conf = conf;
     this.isMapOnly = conf.getNumReduceTasks() == 0;
   }
 
   @Override
-  public void map(AvroWrapper<IN> wrapper, NullWritable value, 
-                  OutputCollector<KO,VO> collector, 
-                  Reporter reporter) throws IOException {
-    if (this.out == null)
-      this.out = new MapCollector<OUT,K,V,KO,VO>(collector, isMapOnly);
+  public void map(AvroWrapper<IN> wrapper, NullWritable value,
+      OutputCollector<KO, VO> collector, Reporter reporter)
+          throws IOException {
+    if (mapper == null) {
+      TaggedInputSplit is = (TaggedInputSplit) reporter.getInputSplit();
+      Class<? extends AvroMapper> mapperClass = is.getMapperClass();
+      mapper = (AvroMapper<IN,OUT>)
+          ReflectionUtils.newInstance(mapperClass, conf);
+    }
+    if (out == null)
+      out = new MapCollector<OUT,K,V,KO,VO>(collector, isMapOnly);
     mapper.map(wrapper.datum(), out, reporter);
   }
-
-  @Override
-  public void close() throws IOException {
-    this.mapper.close();
-  }
-
 }
