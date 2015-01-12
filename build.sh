56838397367fbe3570e3f361034aec24ba9e0f64
Thomas White <tomwhite@apache.org>
2015-01-12 12:11:55 +0000

    AVRO-1537. Make it easier to set up a multi-language build environment.
    git-svn-id: https://svn.apache.org/repos/asf/avro/trunk@1651069 13f79535-47bb-0310-9956-ffa450edef68

--- build.sh
+++ build.sh
@@ -22,7 +22,7 @@ cd `dirname "$0"`				  # connect to root
 VERSION=`cat share/VERSION.txt`
 
 function usage {
-  echo "Usage: $0 {test|dist|sign|clean}"
+  echo "Usage: $0 {test|dist|sign|clean|docker}"
   exit 1
 }
 
@@ -171,6 +171,35 @@ case "$target" in
 
 	(cd lang/perl; [ -f Makefile ] && make clean)
 	;;
+
+    docker)
+        docker build -t avro-build share/docker
+        if [ "$(uname -s)" == "Linux" ]; then
+          USER_NAME=${SUDO_USER:=$USER}
+          USER_ID=$(id -u $USER_NAME)
+          GROUP_ID=$(id -g $USER_NAME)
+        else # boot2docker uid and gid
+          USER_NAME=$USER
+          USER_ID=1000
+          GROUP_ID=50
+        fi
+        docker build -t avro-build-${USER_NAME} - <<UserSpecificDocker
+FROM avro-build
+RUN groupadd -g ${GROUP_ID} ${USER_NAME} || true
+RUN useradd -g ${GROUP_ID} -u ${USER_ID} -k /root -m ${USER_NAME}
+ENV HOME /home/${USER_NAME}
+UserSpecificDocker
+        # By mapping the .m2 directory you can do an mvn install from
+        # within the container and use the result on your normal
+        # system.  And this also is a significant speedup in subsequent
+        # builds because the dependencies are downloaded only once.
+        docker run --rm=true -t -i \
+          -v ${PWD}:/home/${USER_NAME}/avro \
+          -w /home/${USER_NAME}/avro \
+          -v ${HOME}/.m2:/home/${USER_NAME}/.m2 \
+          -u ${USER_NAME} \
+          avro-build-${USER_NAME}
+        ;;
 
     *)
         usage
