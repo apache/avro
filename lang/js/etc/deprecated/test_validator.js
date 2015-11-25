f6e98dca120a31b58d2079e78a0b3bba2ebdc919
Matthieu Monsch <monsch@alum.mit.edu>
2015-11-24 21:53:33 -0800

    AVRO-1747: JavaScript: Add implementation.
    A few features:
    
    + Fast! Typically twice as fast as JSON with much smaller encodings.
    + Full Avro support, including recursive schemas, sort order, and
      evolution.
    + Serialization of arbitrary JavaScript objects via logical types.
    + Unopinionated 64-bit integer compatibility.
    + No dependencies, it even runs in the browser.
    
    The previous API is included with deprecation warnings (this adds
    `underscore` as a dependency).

--- lang/js/etc/deprecated/test_validator.js
+++ lang/js/etc/deprecated/test_validator.js
@@ -13,7 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-var validator = require('../lib/validator.js');
+var validator = require('./validator');
 var Validator = validator.Validator;
 var ProtocolValidator = validator.ProtocolValidator;
 
