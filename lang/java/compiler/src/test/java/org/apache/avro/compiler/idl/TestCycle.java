df57d3ad29918b6c45b6fce15d4f0815024d49f2
Zoltan Farkas <zolyfarkas@yahoo.com>
2016-04-04 11:50:52 -0400

    [fix] use a logger instead of System.out +  reformat to 2 space indentation

--- lang/java/compiler/src/test/java/org/apache/avro/compiler/idl/TestCycle.java
+++ lang/java/compiler/src/test/java/org/apache/avro/compiler/idl/TestCycle.java
@@ -23,7 +23,7 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
-import junit.framework.Assert;
+import org.junit.Assert;
 import org.apache.avro.Protocol;
 import org.apache.avro.Schema;
 import org.apache.avro.compiler.specific.SpecificCompiler;
@@ -36,6 +36,8 @@ import org.apache.avro.io.BinaryEncoder;
 import org.apache.avro.io.DecoderFactory;
 import org.apache.avro.io.EncoderFactory;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -43,63 +45,63 @@ import org.junit.Test;
  */
 public class TestCycle {
 
-    @Test
-    public void testCycleGeneration() throws ParseException, IOException {
-        final ClassLoader cl = Thread.currentThread().getContextClassLoader();
-        Idl idl = new Idl(cl.getResourceAsStream("input/cycle.avdl"),
-                "UTF-8");
-        Protocol protocol = idl.CompilationUnit();
-        String json = protocol.toString();
-        System.out.println(json);
+  private static final Logger LOG = LoggerFactory.getLogger(TestCycle.class);
+  
+  @Test
+  public void testCycleGeneration() throws ParseException, IOException {
+    final ClassLoader cl = Thread.currentThread().getContextClassLoader();
+    Idl idl = new Idl(cl.getResourceAsStream("input/cycle.avdl"),
+            "UTF-8");
+    Protocol protocol = idl.CompilationUnit();
+    String json = protocol.toString();
+    LOG.info(json);
 
-        SpecificCompiler compiler = new SpecificCompiler(protocol);
-        compiler.setStringType(GenericData.StringType.String);
-        File output = new File("./target");
-        compiler.compileToDestination(null, output);
+    SpecificCompiler compiler = new SpecificCompiler(protocol);
+    compiler.setStringType(GenericData.StringType.String);
+    File output = new File("./target");
+    compiler.compileToDestination(null, output);
 
-        Map<String, Schema> schemas = new HashMap<String, Schema>();
-        for (Schema schema : protocol.getTypes()) {
-            final String name = schema.getName();
-            schemas.put(name, schema);
-        }
-
-        GenericRecordBuilder rb2 = new GenericRecordBuilder(schemas.get("SampleNode"));
-        rb2.set("count", 10);
-        rb2.set("subNodes", Collections.EMPTY_LIST);
-        GenericData.Record node = rb2.build();
-
-        GenericRecordBuilder mb = new GenericRecordBuilder(schemas.get("Method"));
-        mb.set("declaringClass", "Test");
-        mb.set("methodName", "test");
-        GenericData.Record method = mb.build();
-
-        GenericRecordBuilder spb = new GenericRecordBuilder(schemas.get("SamplePair"));
-        spb.set("method", method);
-        spb.set("node", node);
-        GenericData.Record sp = spb.build();
-
-
-        GenericRecordBuilder rb = new GenericRecordBuilder(schemas.get("SampleNode"));
-        rb.set("count", 10);
-        rb.set("subNodes", Arrays.asList(sp));
-        GenericData.Record record = rb.build();
-
-        serDeserRecord(record);
-
+    Map<String, Schema> schemas = new HashMap<String, Schema>();
+    for (Schema schema : protocol.getTypes()) {
+      final String name = schema.getName();
+      schemas.put(name, schema);
     }
 
-    private static void serDeserRecord(GenericData.Record data) throws IOException {
-        ByteArrayOutputStream bab = new ByteArrayOutputStream();
-        GenericDatumWriter writer = new GenericDatumWriter(data.getSchema());
-        final BinaryEncoder directBinaryEncoder = EncoderFactory.get().directBinaryEncoder(bab, null);
-        writer.write(data, directBinaryEncoder);
-        directBinaryEncoder.flush();
-        ByteArrayInputStream bis = new ByteArrayInputStream(bab.toByteArray(), 0, bab.size());
-        GenericDatumReader reader = new GenericDatumReader(data.getSchema());
-        BinaryDecoder directBinaryDecoder = DecoderFactory.get().directBinaryDecoder(bis, null);
-        GenericData.Record read = (GenericData.Record) reader.read(null, directBinaryDecoder);
-        Assert.assertEquals(data.toString(), read.toString());
-    }
+    GenericRecordBuilder rb2 = new GenericRecordBuilder(schemas.get("SampleNode"));
+    rb2.set("count", 10);
+    rb2.set("subNodes", Collections.EMPTY_LIST);
+    GenericData.Record node = rb2.build();
 
+    GenericRecordBuilder mb = new GenericRecordBuilder(schemas.get("Method"));
+    mb.set("declaringClass", "Test");
+    mb.set("methodName", "test");
+    GenericData.Record method = mb.build();
+
+    GenericRecordBuilder spb = new GenericRecordBuilder(schemas.get("SamplePair"));
+    spb.set("method", method);
+    spb.set("node", node);
+    GenericData.Record sp = spb.build();
+
+    GenericRecordBuilder rb = new GenericRecordBuilder(schemas.get("SampleNode"));
+    rb.set("count", 10);
+    rb.set("subNodes", Arrays.asList(sp));
+    GenericData.Record record = rb.build();
+
+    serDeserRecord(record);
+
+  }
+
+  private static void serDeserRecord(GenericData.Record data) throws IOException {
+    ByteArrayOutputStream bab = new ByteArrayOutputStream();
+    GenericDatumWriter writer = new GenericDatumWriter(data.getSchema());
+    final BinaryEncoder directBinaryEncoder = EncoderFactory.get().directBinaryEncoder(bab, null);
+    writer.write(data, directBinaryEncoder);
+    directBinaryEncoder.flush();
+    ByteArrayInputStream bis = new ByteArrayInputStream(bab.toByteArray(), 0, bab.size());
+    GenericDatumReader reader = new GenericDatumReader(data.getSchema());
+    BinaryDecoder directBinaryDecoder = DecoderFactory.get().directBinaryDecoder(bis, null);
+    GenericData.Record read = (GenericData.Record) reader.read(null, directBinaryDecoder);
+    Assert.assertEquals(data.toString(), read.toString());
+  }
 
 }
