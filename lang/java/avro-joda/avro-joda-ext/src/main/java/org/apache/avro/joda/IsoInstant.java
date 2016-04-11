5d1905d51f93026f08ec9b3ba94206543488adda
zfarkas <zfarkas@MEZ-RDP-608.PIMCO.IMSWEST.SSCIMS.com>
2016-04-11 10:07:06 -0700

    [add] isoinstant logical type.

---
+++ lang/java/avro-joda/avro-joda-ext/src/main/java/org/apache/avro/joda/IsoInstant.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2014 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.avro.joda;
+
+import java.util.Collections;
+import java.util.Set;
+import org.apache.avro.AbstractLogicalType;
+import org.apache.avro.Schema;
+import org.joda.time.Instant;
+
+public class IsoInstant extends AbstractLogicalType {
+
+  private static final Set<String> RESERVED = Collections.EMPTY_SET;
+
+  public IsoInstant(Schema.Type type) {
+    super(type, RESERVED, "isoinstant", Collections.EMPTY_MAP);
+    // validate the type
+    if (type != Schema.Type.LONG) {
+      throw new IllegalArgumentException(
+              "Logical type " + this + " must be backed by long or string");
+    }
+  }
+
+  @Override
+  public void validate(Schema schema) {
+    // validate the type
+    if (schema.getType() != Schema.Type.LONG) {
+      throw new IllegalArgumentException(
+              "Logical type " + this + " must be backed by long or string");
+    }
+  }
+
+  @Override
+  public Set<String> reserved() {
+    return RESERVED;
+  }
+
+  @Override
+  public Class<?> getLogicalJavaType() {
+    return Instant.class;
+  }
+
+
+  @Override
+  public Object deserialize(Object object) {
+    return new Instant(object);
+  }
+
+  @Override
+  public Object serialize(Object object) {
+    return ((Instant) object).getMillis();
+  }
+
+}
