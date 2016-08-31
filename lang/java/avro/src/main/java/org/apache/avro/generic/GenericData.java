9456db01859dd3619fb09a6d4205ab433b357b02
Doug Cutting <cutting@apache.org>
2016-08-31 16:45:21 -0700

    AVRO-1695. Fix deepCopy() to get correct logical type conversion.

--- lang/java/avro/src/main/java/org/apache/avro/generic/GenericData.java
+++ lang/java/avro/src/main/java/org/apache/avro/generic/GenericData.java
@@ -1030,7 +1030,7 @@ public class GenericData {
     LogicalType logicalType = schema.getLogicalType();
     if (logicalType == null)           // not a logical type -- use raw copy
       return (T)deepCopyRaw(schema, value);
-    Conversion conversion = getConversionByClass(value.getClass());
+    Conversion conversion = getConversionByClass(value.getClass(), logicalType);
     if (conversion == null)            // no conversion defined -- try raw copy
       return (T)deepCopyRaw(schema, value);
     // logical type with conversion: convert to raw, copy, then convert back to logical
