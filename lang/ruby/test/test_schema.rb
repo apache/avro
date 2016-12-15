4b3677c32b879e0e7f717eb95f9135ac654da760
Tim Perkins <tperkins@salsify.com>
2016-12-15 09:35:21 -0500

    AVRO-1969: Add schema compatibility checker for Ruby
    This closes #170
    
    Signed-off-by: Sean Busbey <busbey@apache.org>
    Signed-off-by: sacharya <suraj@apache.org>

--- lang/ruby/test/test_schema.rb
+++ lang/ruby/test/test_schema.rb
@@ -258,4 +258,38 @@ class TestSchema < Test::Unit::TestCase
       }
     assert_equal enum_schema_hash, enum_schema_json.to_avro
   end
+
+def test_empty_record
+    schema = Avro::Schema.parse('{"type":"record", "name":"Empty"}')
+    assert_empty(schema.fields)
+  end
+
+  def test_empty_union
+    schema = Avro::Schema.parse('[]')
+    assert_equal(schema.to_s, '[]')
+  end
+
+  def test_read
+    schema = Avro::Schema.parse('"string"')
+    writer_schema = Avro::Schema.parse('"int"')
+    assert_false(schema.read?(writer_schema))
+    assert_true(schema.read?(schema))
+  end
+
+  def test_be_read
+    schema = Avro::Schema.parse('"string"')
+    writer_schema = Avro::Schema.parse('"int"')
+    assert_false(schema.be_read?(writer_schema))
+    assert_true(schema.be_read?(schema))
+  end
+
+  def test_mutual_read
+    schema = Avro::Schema.parse('"string"')
+    writer_schema = Avro::Schema.parse('"int"')
+    default1 = Avro::Schema.parse('{"type":"record", "name":"Default", "fields":[{"name":"i", "type":"int", "default": 1}]}')
+    default2 = Avro::Schema.parse('{"type":"record", "name":"Default", "fields":[{"name:":"s", "type":"string", "default": ""}]}')
+    assert_false(schema.mutual_read?(writer_schema))
+    assert_true(schema.mutual_read?(schema))
+    assert_true(default1.mutual_read?(default2))
+  end
 end
