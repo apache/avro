ed5d7801e40edd3e34fd499e8b176188a91812d3
Gabor Szadovszky <gabor.szadovszky@cloudera.com>
2016-12-20 14:11:03 +0100

    Set seed for Random to make the unit test deterministic

--- lang/java/trevni/core/src/test/java/org/apache/trevni/TestInputBytes.java
+++ lang/java/trevni/core/src/test/java/org/apache/trevni/TestInputBytes.java
@@ -32,7 +32,7 @@ public class TestInputBytes {
   private static final int COUNT = 100;
 
   @Test public void testRandomReads() throws Exception {
-    Random random = new Random();
+    Random random = new Random(19820210);
     int length = random.nextInt(SIZE) + 1;
     byte[] data = new byte[length];
     random.nextBytes(data);
