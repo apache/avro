ea16b265b866a5fd888296eaaea1f4920f9566e8
Todd Nine <todd@thelastpickle.com>
2017-05-14 15:40:27 -0600

    AVRO-2034. Updates test to show a working record vs a failing record in the simplest possible scheme.

--- lang/java/avro/src/test/java/org/apache/avro/TestNestedRecords.java
+++ lang/java/avro/src/test/java/org/apache/avro/TestNestedRecords.java
@@ -1,5 +1,6 @@
 package org.apache.avro;
 
+import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericDatumReader;
 import org.apache.avro.io.DatumReader;
 import org.apache.avro.io.DecoderFactory;
@@ -9,6 +10,9 @@ import org.junit.Test;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
 /**
  * This test demonstrates the fix for a complex nested schema type.
  */
@@ -16,39 +20,30 @@ public class TestNestedRecords {
 
 
   @Test
-  public void testSubSchema() throws IOException {
+  public void testSingleSubRecord() throws IOException {
 
     final Schema child = SchemaBuilder.record("Child")
-      .namespace("org.apache.avro.nested")
-      .fields()
-      .requiredString("childField").endRecord();
+            .namespace("org.apache.avro.nested")
+            .fields()
+            .requiredString("childField").endRecord();
 
 
     final Schema parent = SchemaBuilder.record("Parent")
-      .namespace("org.apache.avro.nested")
-      .fields()
-      .requiredString("parentField1")
-      .name("child1").type(child).noDefault()
-      .name("child2").type(child).noDefault()
-      .requiredString("parentField2").endRecord();
+            .namespace("org.apache.avro.nested")
+            .fields()
+            .requiredString("parentField1")
+            .name("child1").type(child).noDefault()
+            .requiredString("parentField2").endRecord();
+
 
 
     final String inputAsExpected = "{\n" +
-      " \"parentField1\": \"parentValue\",\n" +
-      " \"child1\":{\n" +
-      "    \"childField\":\"childValue\",\n" +
-
-      //this field should be safely ignored
-      "    \"extraField\":\"extraValue\"\n" +
-      " },\n" +
-      " \"child2\":{\n" +
-      "    \"childField\":\"childValue\",\n" +
-
-      //This field should be safely ignored
-      "    \"extraField\":\"extraValue\"\n" +
-      " },\n" +
-      " \"parentField2\":\"parentValue2\"\n" +
-      "}";
+            " \"parentField1\": \"parentValue1\",\n" +
+            " \"child1\":{\n" +
+            "    \"childField\":\"childValue1\"\n" +
+            " },\n" +
+            " \"parentField2\":\"parentValue2\"\n" +
+            "}";
 
 
     final ByteArrayInputStream inputStream = new ByteArrayInputStream(inputAsExpected.getBytes());
@@ -56,14 +51,60 @@ public class TestNestedRecords {
     final JsonDecoder decoder = DecoderFactory.get().jsonDecoder(parent, inputStream);
     final DatumReader<Object> reader = new GenericDatumReader<Object>(parent);
 
-    final Object decoded = reader.read(null, decoder);
+    final GenericData.Record  decoded = (GenericData.Record) reader.read(null, decoder);
 
-//    assertThat(parent.getParentField(), equalTo("parentValue"));
-//    assertThat(parent.getChild().getChildField(), equalTo("childValue"));
 
-    System.out.println(decoded);
+    assertThat(decoded.get("parentField1").toString(), equalTo("parentValue1"));
+    assertThat(decoded.get("parentField2").toString(), equalTo("parentValue2"));
+
+    assertThat(((GenericData.Record)decoded.get("child1")).get("childField").toString(), equalTo("childValue1"));
 
   }
 
+
+
+  @Test
+  public void testSingleSubRecordExtraField() throws IOException {
+
+    final Schema child = SchemaBuilder.record("Child")
+            .namespace("org.apache.avro.nested")
+            .fields()
+            .requiredString("childField").endRecord();
+
+
+    final Schema parent = SchemaBuilder.record("Parent")
+            .namespace("org.apache.avro.nested")
+            .fields()
+            .requiredString("parentField1")
+            .name("child1").type(child).noDefault()
+            .requiredString("parentField2").endRecord();
+
+
+    final String inputAsExpected = "{\n" +
+            " \"parentField1\": \"parentValue1\",\n" +
+            " \"child1\":{\n" +
+            "    \"childField\":\"childValue1\",\n" +
+
+            //this field should be safely ignored
+            "    \"extraField\":\"extraValue\"\n" +
+            " },\n" +
+            " \"parentField2\":\"parentValue2\"\n" +
+            "}";
+
+
+    final ByteArrayInputStream inputStream = new ByteArrayInputStream(inputAsExpected.getBytes());
+
+    final JsonDecoder decoder = DecoderFactory.get().jsonDecoder(parent, inputStream);
+    final DatumReader<Object> reader = new GenericDatumReader<Object>(parent);
+
+    final GenericData.Record decoded = (GenericData.Record) reader.read(null, decoder);
+
+    assertThat(decoded.get("parentField1").toString(), equalTo("parentValue1"));
+    assertThat(decoded.get("parentField2").toString(), equalTo("parentValue2"));
+
+    assertThat(((GenericData.Record)decoded.get("child1")).get("childField").toString(), equalTo("childValue1"));
+
+
+  }
 
 }
