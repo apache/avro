fc68a74d0b94329bef8330e042a147bdb18d7929
Pepijn Noltes <pepijnnoltes@gmail.com>
2017-06-08 21:56:14 +0200

    AVRO-2016: Refactors the IdlTool. The command line is now parsed with use of the apache commons cli

--- lang/java/tools/src/main/java/org/apache/avro/tool/IdlTool.java
+++ lang/java/tools/src/main/java/org/apache/avro/tool/IdlTool.java
@@ -18,108 +18,147 @@
 
 package org.apache.avro.tool;
 
+
 import org.apache.avro.Protocol;
 import org.apache.avro.compiler.idl.Idl;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.Options;
+
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Tool implementation for generating Avro JSON schemata from
  * idl format files.
  */
 public class IdlTool implements Tool {
-  @Override
-  public int run(InputStream in, PrintStream out, PrintStream err,
-                  List<String> args) throws Exception {
 
-    PrintStream parseOut = out;
+    private final Option help = Option.builder("h").longOpt("help")
+            .desc("Display help information").build();
 
-    boolean incorrectArgsOrHelpRequested = false;
-    List<File> includeDirs = new ArrayList<File>();
-    String fileIn = null;
-    String fileOut = null;
+    private final Option include = Option.builder("i").longOpt("include")
+            .hasArg(true).argName("include_dir")
+            .desc("Includes the specified include dir for import resolvement")
+            .build();
 
-    for (int arg = 0; arg < args.size(); ++arg) {
-        if ("--help".equals(args.get(arg))) {
-            incorrectArgsOrHelpRequested = true;
-            break;
-        } else if ("--include".equals(args.get(arg))) {
-            arg += 1;
-            String fName = arg < args.size() ? args.get(arg) : "";
-            File f = new File(fName);
-            if (f.exists() && f.isDirectory()) {
-                includeDirs.add(f);
-            } else {
-                err.println("Specified --include path '" + fName + "' does not exists or is not a directory!");
-                incorrectArgsOrHelpRequested = true;
-                break;
-            }
-        } else /*positional argument*/ {
-            if (fileIn == null) {
-                fileIn = args.get(arg);
-                if (!"-".equals(fileIn)) {
-                    File f = new File(fileIn);
-                    if (!f.exists() || f.isDirectory()) {
-                        err.println("Specified input path is not an existing file or is a directory!");
-                        incorrectArgsOrHelpRequested = true;
-                        break;
-                    }
-                }
-            } else if (fileOut == null) {
-                fileOut = args.get(arg);
-            } else {
-                incorrectArgsOrHelpRequested = true;
-                break;
+    private final Options options = new Options()
+            .addOption(help)
+            .addOption(include);
+
+    private final CommandLineParser parser = new DefaultParser();
+    private final HelpFormatter formatter = new HelpFormatter();
+
+    @Override
+    public int run(InputStream in, PrintStream out, PrintStream err, List<String> args) throws Exception {
+        CommandLine line = parser.parse(options, args.toArray(new String[]{}));
+
+        if (line.hasOption(help.getOpt())) {
+            printHelp();
+            return 0;
+        }
+
+        String fileIn = "-";
+        String fileOut = "-";
+        List<File> includeDirs = new ArrayList<File>();
+
+        if (line.hasOption(include.getOpt())) {
+            for (String inclDir : line.getOptionValues(include.getOpt())) {
+                includeDirs.add(new File(inclDir));
             }
         }
+        if (line.getArgs().length >= 1) {
+            fileIn = line.getArgs()[0];
+        }
+        if (line.getArgs().length >= 2) {
+            fileOut = line.getArgs()[1];
+        }
+
+        //Check arguments
+        int rc = 0;
+        for (File dir : includeDirs) {
+            if (!dir.exists() || !dir.isDirectory()) {
+                System.err.println("");
+                System.err.println("Error: Specified " + include.getArgName() + " path '" + dir + "' does not exists or is not a directory!");
+                System.err.println("");
+                rc = -1;
+            }
+        }
+        if (!"-".equals(fileIn)) {
+            File f = new File(fileIn);
+            if (!f.exists() || f.isDirectory()) {
+                System.err.println("");
+                System.err.println("Error: Specified input file '" + fileIn + "' does not exists or is a directory!");
+                System.err.println("");
+                rc = -1;
+            }
+        }
+        if (!"-".equals(fileOut)) {
+            File f = new File(fileOut);
+            if (!f.exists() || f.isDirectory()) {
+                System.err.println("");
+                System.err.println("Error: Specified output file '" + fileOut + "' does not exists or is a directory!");
+                System.err.println("");
+                rc = -1;
+            }
+        }
+
+        if (rc != 0) {
+            printHelp();
+        } else {
+            Idl parser = null;
+            if ("-".equals(fileIn)) {
+                parser = new Idl(in);
+            } else {
+                parser = new Idl(new File(fileIn));
+            }
+
+            PrintStream parseOut = out;
+            if (!"-".equals(fileOut)) {
+                parseOut = new PrintStream(new FileOutputStream(fileOut));
+            }
+
+            for (File includeDir : includeDirs) {
+                parser.addIncludeDir(includeDir);
+            }
+
+            Protocol p = parser.CompilationUnit();
+            parseOut.print(p.toString(true));
+        }
+
+        return rc;
     }
 
+    private void printHelp() {
+        StringBuffer header = new StringBuffer();
+        header.append("If an output path is not specified, outputs to stdout.");
+        header.append(System.getProperty("line.separator"));
+        header.append("If no input or output is specified, takes input from stdin and outputs to stdout.");
+        header.append(System.getProperty("line.separator"));
+        header.append("The special path \"-\" may also be specified to refer to stdin and stdout.");
+        header.append(System.getProperty("line.separator"));
+        header.append(System.getProperty("line.separator"));
 
-    if (incorrectArgsOrHelpRequested) {
-      err.println("Usage: idl [--include includeDir ...] [in] [out]");
-      err.println("");
-      err.println("If an output path is not specified, outputs to stdout.");
-      err.println("If no input or output is specified, takes input from");
-      err.println("stdin and outputs to stdin.");
-      err.println("The special path \"-\" may also be specified to refer to");
-      err.println("stdin and stdout.");
-      err.println("If any --include options are specified. The specified");
-      err.println("include dirs are also used to resolve a import statement");
-      return -1;
+        formatter.printHelp(
+                "idl [--include <include> ...] [in] [out]",
+                header.toString(), options, null);
     }
 
-    Idl parser;
-    if (fileIn != null && ! "-".equals(fileIn)) {
-      parser = new Idl(new File(fileIn));
-    } else {
-      parser = new Idl(in);
+    @Override
+    public String getName() {
+        return "idl";
     }
 
-    if (fileOut != null && ! "-".equals(fileOut)) {
-      parseOut = new PrintStream(new FileOutputStream(fileOut));
+    @Override
+    public String getShortDescription() {
+        return "Generates a JSON schema from an Avro IDL file";
     }
-
-    for (File includeDir : includeDirs) {
-        parser.addIncludeDir(includeDir);
-    }
-
-    Protocol p = parser.CompilationUnit();
-    parseOut.print(p.toString(true));
-    return 0;
-  }
-
-  @Override
-  public String getName() {
-    return "idl";
-  }
-
-  @Override
-  public String getShortDescription() {
-    return "Generates a JSON schema from an Avro IDL file";
-  }
 }
