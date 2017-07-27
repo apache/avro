3801888fa8c9dc96a1613e41cbc33800bc84f1b4
BELUGA BEHR <dam6923@gmail.com>
2017-07-27 09:42:13 +0200

    AVRO-2056: DirectBinaryEncoder Creates Buffer For Each Call To writeDouble

--- lang/java/avro/src/main/java/org/apache/avro/io/DirectBinaryEncoder.java
+++ lang/java/avro/src/main/java/org/apache/avro/io/DirectBinaryEncoder.java
@@ -110,7 +110,6 @@ public class DirectBinaryEncoder extends BinaryEncoder {
 
   @Override
   public void writeDouble(double d) throws IOException {
-    byte[] buf = new byte[8];
     int len = BinaryData.encodeDouble(d, buf, 0);
     out.write(buf, 0, len);
   }
