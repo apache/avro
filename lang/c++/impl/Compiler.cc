51a92fea4ad197c6f8ddd91cefc1e35840c15c4a
Victor Mota <vimota@gmail.com>
2017-08-19 18:56:03 -0700

    AVRO-1335: Adds C++ support for default values in schema serialization to json.

--- lang/c++/impl/Compiler.cc
+++ lang/c++/impl/Compiler.cc
@@ -347,7 +347,7 @@ static NodePtr makeFixedNode(const Entity& e,
 {
     int v = static_cast<int>(getLongField(e, m, "size"));
     if (v <= 0) {
-        throw Exception(boost::format("Size for fixed is not positive: ") %
+        throw Exception(boost::format("Size for fixed is not positive: %1%") %
             e.toString());
     }
     return NodePtr(new NodeFixed(asSingleAttribute(name),
