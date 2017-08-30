c642adaf3d29183049cd4ad2e0012b002d95d7c6
Daniil Guitelson <dguitelson@openwaygroup.com>
2017-08-30 18:37:41 +0300

    AVRO-2069: Use primitive fields in generated getters & setters in Java code
    Fix broken unit tests in TestSpecificCompilerTool

--- lang/java/tools/src/test/compiler/output/Player.java
+++ lang/java/tools/src/test/compiler/output/Player.java
@@ -107,7 +107,7 @@ public class Player extends org.apache.avro.specific.SpecificRecordBase implemen
    * Gets the value of the 'number' field.
    * @return The number of the player
    */
-  public java.lang.Integer getNumber() {
+  public int getNumber() {
     return number;
   }
 
@@ -116,7 +116,7 @@ public class Player extends org.apache.avro.specific.SpecificRecordBase implemen
    * The number of the player
    * @param value the value to set.
    */
-  public void setNumber(java.lang.Integer value) {
+  public void setNumber(int value) {
     this.number = value;
   }
 
@@ -272,7 +272,7 @@ public class Player extends org.apache.avro.specific.SpecificRecordBase implemen
       * The number of the player
       * @return The value.
       */
-    public java.lang.Integer getNumber() {
+    public int getNumber() {
       return number;
     }
 
