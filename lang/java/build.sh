8ae9a98995c13eabef361a0fdfec2e4995ee20d4
Gabor Szadovszky <gabor@apache.org>
2017-09-01 15:33:28 +0200

    AVRO-2059: Changes according to busbey's comments

--- lang/java/build.sh
+++ lang/java/build.sh
@@ -38,9 +38,6 @@ for target in "$@"
 do
 
 function do_dist() {
-  mvn package -DskipTests;
-  rm -rf mapred/target/{classes,test-classes}/;
-  rm -rf trevni/avro/target/{classes,test-classes}/;
   mvn -P dist package -DskipTests -Davro.version=$VERSION javadoc:aggregate
 }
 
