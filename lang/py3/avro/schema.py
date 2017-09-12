e3730b9ae7194b7292c5879f253f50541aa194a1
Robert Smallshire <rob@sixty-north.com>
2017-09-12 15:21:16 +0200

    AVRO-2104: Schema normalization and fingerprinting for Python 3.
    An implementation of schema normalization and fingerprinting for Python
    3. The code is a fairly literal translation of the Java code and is
    entirely additional to the existing code, except for one small change
    in the way schema.Name is implemented to improve compatibility with the
    Java version.
    
    The new implementation is covered by 138 unit tests which have been
    automatically generated by a script (not provided) which executed the
    Java Avro normalization and fingerprinting code to provide expected
    values for the Python implementation to reproduce.

--- lang/py3/avro/schema.py
+++ lang/py3/avro/schema.py
@@ -40,12 +40,11 @@ A schema may be one of:
 
 
 import abc
-import collections
 import json
 import logging
 import re
 
-logger = logging.getLogger(__name__) 
+logger = logging.getLogger(__name__)
 
 # ------------------------------------------------------------------------------
 # Constants
@@ -318,7 +317,9 @@ class Name(object):
       # name is relative, combine with explicit namespace:
       self._name = name
       self._namespace = namespace
-      self._fullname = '%s.%s' % (self._namespace, self._name)
+      self._fullname = (self._name
+                        if (not self._namespace) else
+                        '%s.%s' % (self._namespace, self._name))
 
       # Validate the fullname:
       if _RE_FULL_NAME.match(self._fullname) is None:
@@ -328,8 +329,8 @@ class Name(object):
 
   def __eq__(self, other):
     if not isinstance(other, Name):
-      return False
-    return (self.fullname == other.fullname)
+      return NotImplemented
+    return self.fullname == other.fullname
 
   @property
   def simple_name(self):
@@ -343,7 +344,7 @@ class Name(object):
 
   @property
   def fullname(self):
-    """Returns: the full name (always contains a period '.')."""
+    """Returns: the full name."""
     return self._fullname
 
 
