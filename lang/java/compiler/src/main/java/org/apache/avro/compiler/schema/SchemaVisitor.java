41653853c66143796e0bac503064e24cc0353a87
Niels Basjes <nbasjes@bol.com>
2017-12-22 10:48:40 +0100

    AVRO-2118: Remove @author tag. Dangling JavaDoc fix.

--- lang/java/compiler/src/main/java/org/apache/avro/compiler/schema/SchemaVisitor.java
+++ lang/java/compiler/src/main/java/org/apache/avro/compiler/schema/SchemaVisitor.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -19,9 +19,6 @@ package org.apache.avro.compiler.schema;
 
 import org.apache.avro.Schema;
 
-/**
- * @author zoly
- */
 public interface SchemaVisitor<T> {
 
   /**
--- lang/java/compiler/src/main/java/org/apache/avro/compiler/schema/SchemaVisitorAction.java
+++ lang/java/compiler/src/main/java/org/apache/avro/compiler/schema/SchemaVisitorAction.java
@@ -1,4 +1,4 @@
-/**
+/*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -18,9 +18,6 @@
 
 package org.apache.avro.compiler.schema;
 
-/**
- * @author zoly
- */
 public enum SchemaVisitorAction {
 
     /**
