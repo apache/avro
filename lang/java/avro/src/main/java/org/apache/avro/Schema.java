a7a43da6de9bd44e57e057319127c64b2fca56a0
Niels Basjes <nbasjes@bol.com>
2017-12-23 13:24:52 +0100

    AVRO-2120: Fix NullPointerException thrown by Schema.Parser#parse("")

--- lang/java/avro/src/main/java/org/apache/avro/Schema.java
+++ lang/java/avro/src/main/java/org/apache/avro/Schema.java
@@ -1225,6 +1225,9 @@ public abstract class Schema extends JsonProperties {
 
   /** @see #parse(String) */
   static Schema parse(JsonNode schema, Names names) {
+    if (schema == null) {
+      throw new SchemaParseException("Cannot parse <null> schema");
+    }
     if (schema.isTextual()) {                     // name
       Schema result = names.get(schema.getTextValue());
       if (result == null)
