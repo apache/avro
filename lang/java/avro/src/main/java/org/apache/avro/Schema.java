fbca0c529d7e345dacabc2f693114c980fd08a2a
jmaxwell <jmaxwell@theice.com>
2018-01-17 16:54:39 -0600

    AVRO-2131 Modified Schema.parse(JsonNode schema, Names names) to allow Unions with self references.

--- lang/java/avro/src/main/java/org/apache/avro/Schema.java
+++ lang/java/avro/src/main/java/org/apache/avro/Schema.java
@@ -1318,8 +1318,13 @@ public abstract class Schema extends JsonProperties {
           throw new SchemaParseException("Invalid or no size: "+schema);
         result = new FixedSchema(name, doc, sizeNode.getIntValue());
         if (name != null) names.add(result);
-      } else
+      } else {  //For unions with self reference
+        Name nameFromType = new Name(type, names.space);
+        if (names.containsKey(nameFromType)) {
+          return names.get(nameFromType);
+        }
         throw new SchemaParseException("Type not supported: "+type);
+      }
       Iterator<String> i = schema.getFieldNames();
       while (i.hasNext()) {                       // add properties
         String prop = i.next();
