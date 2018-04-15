660fc96b6c7ac53b73f50319d1552d4aa42fdc38
Brian Lachniet <blachniet@gmail.com>
2018-04-15 14:18:33 -0400

    AVRO-2112: Move C# IPC tests to a Avro.ipc.test

--- lang/csharp/src/apache/ipc.test/MailResponder.cs
+++ lang/csharp/src/apache/ipc.test/MailResponder.cs
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
@@ -46,7 +46,7 @@ namespace Avro.Test.Ipc
                     string readAllLines;
                     using (
                         Stream stream =
-                            Assembly.GetExecutingAssembly().GetManifestResourceStream("Avro.test.Ipc.mail.avpr"))
+                            Assembly.GetExecutingAssembly().GetManifestResourceStream("Avro.ipc.test.mail.avpr"))
                     using (var reader = new StreamReader(stream))
                     {
                         readAllLines = reader.ReadToEnd();
