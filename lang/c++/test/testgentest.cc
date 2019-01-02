7da2a4f05f80a044a1d5033048d0040a9f7f2445
Thiruvalluvan M G <thiru@startsmartlabs.com>
2019-01-02 18:36:23 +0530

    Fixed a few warnings in C++

--- lang/c++/test/testgentest.cc
+++ lang/c++/test/testgentest.cc
@@ -22,6 +22,10 @@
 #include <sstream>
 #include <boost/test/included/unit_test_framework.hpp>
 
+#if defined(__clang__)
+#pragma clang diagnostic ignored "-Winvalid-offsetof"
+#endif
+
 #include "testgen.hh" // < generated header
 #include "testgen2.hh" // < generated header
 
@@ -553,4 +557,3 @@ init_unit_test_suite( int argc, char* argv[] )
 
     return test;
 }
-
