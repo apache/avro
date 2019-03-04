4a3609cd854e9ef5bdda5b43e188a3309a93aef9
ivangreene <igreene@fanthreesixty.com>
2019-03-04 16:44:48 -0600

    AVRO-1810: Fix GenericDatumWriter w/ enums (#462)

--- lang/java/avro/src/test/java/org/apache/avro/TypeEnum.java
+++ lang/java/avro/src/test/java/org/apache/avro/TypeEnum.java
@@ -22,8 +22,9 @@
 package org.apache.avro;
 @SuppressWarnings("all")
 @org.apache.avro.specific.AvroGenerated
-public enum TypeEnum {
+public enum TypeEnum implements org.apache.avro.generic.GenericEnumSymbol<TypeEnum> {
   a, b, c  ;
   public static final org.apache.avro.Schema SCHEMA$ = new org.apache.avro.Schema.Parser().parse("{\"type\":\"enum\",\"name\":\"TypeEnum\",\"namespace\":\"org.apache.avro\",\"symbols\":[\"a\",\"b\",\"c\"]}");
   public static org.apache.avro.Schema getClassSchema() { return SCHEMA$; }
+  public org.apache.avro.Schema getSchema() { return SCHEMA$; }
 }
