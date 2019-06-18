4988b9b701a99561f49a0e5147f08d2dbec0ab3d
Brian Lachniet <blachniet@gmail.com>
2019-06-17 21:39:58 -0400

    AVRO-2422: Build NuGet packages on Linux (#553)
    - Primarily target .NET Standard and .NET Core
    - Still test against net40 & netcore when building/testing on Windows
    - Package Avrogen as a dotnet tool
    - Upgrade to .NET Core 2.2 (previously 2.0)

--- lang/csharp/src/apache/test/Specific/SpecificTests.cs
+++ lang/csharp/src/apache/test/Specific/SpecificTests.cs
@@ -31,7 +31,7 @@ namespace Avro.Test
     [TestFixture]
     class SpecificTests
     {
-#if !NETCOREAPP2_0 // System.CodeDom compilation not supported in .NET Core: https://github.com/dotnet/corefx/issues/12180
+#if !NETCOREAPP2_2 // System.CodeDom compilation not supported in .NET Core: https://github.com/dotnet/corefx/issues/12180
         // The dynamically created assembly used in the test below can only be created
         // once otherwise repeated tests will fail as the same type name will exist in
         // multiple assemblies and so the type in the test and the type found by ObjectCreator
@@ -189,7 +189,7 @@ namespace Avro.Test
 
 
             // compile
-            var comparam = new CompilerParameters(new string[] { "mscorlib.dll" });
+            var comparam = new CompilerParameters(new string[] { "netstandard.dll" });
             comparam.ReferencedAssemblies.Add("System.dll");
             comparam.ReferencedAssemblies.Add(Path.Combine(TestContext.CurrentContext.TestDirectory, "Avro.dll"));
             comparam.GenerateInMemory = true;
