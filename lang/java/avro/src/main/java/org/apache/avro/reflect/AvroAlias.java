d8d73c8ea74268cd6d98d5ea9f83774d371ffed6
baunz <johannes.schulte@gmail.com>
2019-06-18 21:42:23 +0200

    AVRO-2421 Allow multiple @AvroAliases annotations on classes and fields (#544)

--- lang/java/avro/src/main/java/org/apache/avro/reflect/AvroAlias.java
+++ lang/java/avro/src/main/java/org/apache/avro/reflect/AvroAlias.java
@@ -18,6 +18,7 @@
 package org.apache.avro.reflect;
 
 import java.lang.annotation.ElementType;
+import java.lang.annotation.Repeatable;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
@@ -28,6 +29,7 @@ import java.lang.annotation.Target;
  */
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ ElementType.TYPE, ElementType.FIELD })
+@Repeatable(AvroAliases.class)
 public @interface AvroAlias {
   String NULL = "NOT A VALID NAMESPACE";
 
