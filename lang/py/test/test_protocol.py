169f0c4aedcc3714ba88ef943eaa30a5f28cbada
Michael A. Smith <michaels@syapse.com>
2019-06-23 15:33:33 -0400

    AVRO-2410: Make test_protocol whitespace manageable

--- lang/py/test/test_protocol.py
+++ lang/py/test/test_protocol.py
@@ -16,16 +16,17 @@
 """
 Test the protocol parsing logic.
 """
+import json
 import unittest
 
 from avro import protocol
 
 
 class ExampleProtocol(object):
-    def __init__(self, protocol_string, valid, name='', comment=''):
-        self._protocol_string = protocol_string
+    def __init__(self, protocol_dict, valid, name='', comment=''):
+        self._protocol_string = json.dumps(protocol_dict)
         self._valid = valid
-        self._name = name or protocol_string  # default to schema_string for name
+        self._name = name or self._protocol_string  # default to schema_string for name
         self._comment = comment
 
     # read-only properties
@@ -41,310 +42,264 @@ class ExampleProtocol(object):
 #
 # Example Protocols
 #
-HELLO_WORLD = ExampleProtocol("""\
-{
-  "namespace": "com.acme",
-  "protocol": "HelloWorld",
-
-  "types": [
-    {"name": "Greeting", "type": "record", "fields": [
-      {"name": "message", "type": "string"}]},
-    {"name": "Curse", "type": "error", "fields": [
-      {"name": "message", "type": "string"}]}
-  ],
-
-  "messages": {
-    "hello": {
-      "request": [{"name": "greeting", "type": "Greeting" }],
-      "response": "Greeting",
-      "errors": ["Curse"]
+HELLO_WORLD = ExampleProtocol({
+    "namespace": "com.acme",
+    "protocol": "HelloWorld",
+    "types": [
+        {"name": "Greeting", "type": "record", "fields": [
+            {"name": "message", "type": "string"}]},
+        {"name": "Curse", "type": "error", "fields": [
+            {"name": "message", "type": "string"}]}
+    ],
+    "messages": {
+        "hello": {
+            "request": [{"name": "greeting", "type": "Greeting"}],
+            "response": "Greeting",
+            "errors": ["Curse"]
+        }
     }
-  }
-}
-    """, True)
-EXAMPLES = [
-  HELLO_WORLD,
-  ExampleProtocol("""\
-{"namespace": "org.apache.avro.test",
- "protocol": "Simple",
+}, True)
 
- "types": [
-     {"name": "Kind", "type": "enum", "symbols": ["FOO","BAR","BAZ"]},
+TEST_SIMPLE = ExampleProtocol({
+    "namespace": "org.apache.avro.test",
+    "protocol": "Simple",
+    "types": [
+        {"name": "Kind", "type": "enum", "symbols": ["FOO", "BAR", "BAZ"]},
+        {"name": "MD5", "type": "fixed", "size": 16},
+        {"name": "TestRecord", "type": "record",
+         "fields": [
+             {"name": "name", "type": "string", "order": "ignore"},
+             {"name": "kind", "type": "Kind", "order": "descending"},
+             {"name": "hash", "type": "MD5"}
+         ]},
+        {"name": "TestError", "type": "error",
+         "fields": [{"name": "message", "type": "string"}]}
+    ],
+    "messages": {
+        "hello": {
+            "request": [{"name": "greeting", "type": "string"}],
+            "response": "string"
+        },
+        "echo": {
+            "request": [{"name": "record", "type": "TestRecord"}],
+            "response": "TestRecord"
+        },
+        "add": {
+            "request": [{"name": "arg1", "type": "int"}, {"name": "arg2", "type": "int"}],
+            "response": "int"
+        },
+        "echoBytes": {
+            "request": [{"name": "data", "type": "bytes"}],
+            "response": "bytes"
+        },
+        "error": {
+            "request": [],
+            "response": "null",
+            "errors": ["TestError"]
+        }
+    }
+}, True)
 
-     {"name": "MD5", "type": "fixed", "size": 16},
+TEST_NAMESPACE = ExampleProtocol({
+    "namespace": "org.apache.avro.test.namespace",
+    "protocol": "TestNamespace",
+    "types": [
+        {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
+        {"name": "TestRecord", "type": "record",
+         "fields": [{"name": "hash", "type": "org.apache.avro.test.util.MD5"}]
+         },
+        {"name": "TestError", "namespace": "org.apache.avro.test.errors",
+         "type": "error", "fields": [{"name": "message", "type": "string"}]
+         }
+    ],
+    "messages": {
+        "echo": {
+            "request": [{"name": "record", "type": "TestRecord"}],
+            "response": "TestRecord"
+        },
+        "error": {
+            "request": [],
+            "response": "null",
+            "errors": ["org.apache.avro.test.errors.TestError"]
+        }
+    }
+}, True)
 
-     {"name": "TestRecord", "type": "record",
-      "fields": [
-          {"name": "name", "type": "string", "order": "ignore"},
-          {"name": "kind", "type": "Kind", "order": "descending"},
-          {"name": "hash", "type": "MD5"}
-      ]
-     },
+TEST_IMPLICIT_NAMESPACE = ExampleProtocol({
+    "namespace": "org.apache.avro.test.namespace",
+    "protocol": "TestImplicitNamespace",
+    "types": [
+        {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
+        {"name": "ReferencedRecord", "type": "record",
+         "fields": [{"name": "foo", "type": "string"}]},
+        {"name": "TestRecord", "type": "record",
+         "fields": [{"name": "hash", "type": "org.apache.avro.test.util.MD5"},
+                    {"name": "unqalified", "type": "ReferencedRecord"}]
+         },
+        {"name": "TestError",
+         "type": "error", "fields": [{"name": "message", "type": "string"}]
+         }
+    ],
+    "messages": {
+        "echo": {
+            "request": [{"name": "qualified",
+                         "type": "org.apache.avro.test.namespace.TestRecord"}],
+            "response": "TestRecord"
+        },
+        "error": {
+            "request": [],
+            "response": "null",
+            "errors": ["org.apache.avro.test.namespace.TestError"]
+        }
+    }
+}, True)
 
-     {"name": "TestError", "type": "error", "fields": [
-         {"name": "message", "type": "string"}
-      ]
-     }
+TEST_NAMESPACE_TWO = ExampleProtocol({
+    "namespace": "org.apache.avro.test.namespace",
+    "protocol": "TestNamespaceTwo",
+    "types": [
+        {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
+        {"name": "ReferencedRecord", "type": "record",
+         "namespace": "org.apache.avro.other.namespace",
+         "fields": [{"name": "foo", "type": "string"}]},
+        {"name": "TestRecord", "type": "record",
+         "fields": [{"name": "hash", "type": "org.apache.avro.test.util.MD5"},
+                    {"name": "qualified",
+                     "type": "org.apache.avro.other.namespace.ReferencedRecord"}
+                    ]
+         },
+        {"name": "TestError",
+         "type": "error", "fields": [{"name": "message", "type": "string"}]
+         }
+    ],
+    "messages": {
+        "echo": {
+            "request": [{"name": "qualified",
+                         "type": "org.apache.avro.test.namespace.TestRecord"}],
+            "response": "TestRecord"
+        },
+        "error": {
+            "request": [],
+            "response": "null",
+            "errors": ["org.apache.avro.test.namespace.TestError"]
+        }
+    }
+}, True)
 
- ],
+TEST_VALID_REPEATED_NAME = ExampleProtocol({
+    "namespace": "org.apache.avro.test.namespace",
+    "protocol": "TestValidRepeatedName",
+    "types": [
+        {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
+        {"name": "ReferencedRecord", "type": "record",
+         "namespace": "org.apache.avro.other.namespace",
+         "fields": [{"name": "foo", "type": "string"}]},
+        {"name": "ReferencedRecord", "type": "record",
+         "fields": [{"name": "bar", "type": "double"}]},
+        {"name": "TestError",
+         "type": "error", "fields": [{"name": "message", "type": "string"}]
+         }
+    ],
+    "messages": {
+        "echo": {
+            "request": [{"name": "qualified",
+                         "type": "ReferencedRecord"}],
+            "response": "org.apache.avro.other.namespace.ReferencedRecord"
+        },
+        "error": {
+            "request": [],
+            "response": "null",
+            "errors": ["org.apache.avro.test.namespace.TestError"]
+        }
+    }
+}, True)
 
- "messages": {
+TEST_INVALID_REPEATED_NAME = ExampleProtocol({
+    "namespace": "org.apache.avro.test.namespace",
+    "protocol": "TestInvalidRepeatedName",
+    "types": [
+        {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
+        {"name": "ReferencedRecord", "type": "record",
+         "fields": [{"name": "foo", "type": "string"}]},
+        {"name": "ReferencedRecord", "type": "record",
+         "fields": [{"name": "bar", "type": "double"}]},
+        {"name": "TestError",
+         "type": "error", "fields": [{"name": "message", "type": "string"}]
+         }
+    ],
+    "messages": {
+        "echo": {
+            "request": [{"name": "qualified",
+                         "type": "ReferencedRecord"}],
+            "response": "org.apache.avro.other.namespace.ReferencedRecord"
+        },
+        "error": {
+            "request": [],
+            "response": "null",
+            "errors": ["org.apache.avro.test.namespace.TestError"]
+        }
+    }
+}, False)
 
-     "hello": {
-         "request": [{"name": "greeting", "type": "string"}],
-         "response": "string"
-     },
+TEST_BULK_DATA = ExampleProtocol({
+    "namespace": "org.apache.avro.test",
+    "protocol": "BulkData",
+    "types": [],
+    "messages": {
+        "read": {
+            "request": [],
+            "response": "bytes"
+        },
+        "write": {
+            "request": [{"name": "data", "type": "bytes"}],
+            "response": "null"
+        }
+    }
+}, True)
 
-     "echo": {
-         "request": [{"name": "record", "type": "TestRecord"}],
-         "response": "TestRecord"
-     },
-
-     "add": {
-         "request": [{"name": "arg1", "type": "int"}, {"name": "arg2", "type": "int"}],
-         "response": "int"
-     },
-
-     "echoBytes": {
-         "request": [{"name": "data", "type": "bytes"}],
-         "response": "bytes"
-     },
-
-     "error": {
-         "request": [],
-         "response": "null",
-         "errors": ["TestError"]
-     }
- }
-
-}
-    """, True),
-  ExampleProtocol("""\
-{"namespace": "org.apache.avro.test.namespace",
- "protocol": "TestNamespace",
-
- "types": [
-     {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
-     {"name": "TestRecord", "type": "record",
-      "fields": [ {"name": "hash", "type": "org.apache.avro.test.util.MD5"} ]
-     },
-     {"name": "TestError", "namespace": "org.apache.avro.test.errors",
-      "type": "error", "fields": [ {"name": "message", "type": "string"} ]
-     }
- ],
-
- "messages": {
-     "echo": {
-         "request": [{"name": "record", "type": "TestRecord"}],
-         "response": "TestRecord"
-     },
-
-     "error": {
-         "request": [],
-         "response": "null",
-         "errors": ["org.apache.avro.test.errors.TestError"]
-     }
-
- }
-
-}
-    """, True),
-    ExampleProtocol("""\
-{"namespace": "org.apache.avro.test.namespace",
- "protocol": "TestImplicitNamespace",
-
- "types": [
-     {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
-     {"name": "ReferencedRecord", "type": "record",
-       "fields": [ {"name": "foo", "type": "string"} ] },
-     {"name": "TestRecord", "type": "record",
-      "fields": [ {"name": "hash", "type": "org.apache.avro.test.util.MD5"},
-                  {"name": "unqalified", "type": "ReferencedRecord"} ]
-     },
-     {"name": "TestError",
-      "type": "error", "fields": [ {"name": "message", "type": "string"} ]
-     }
- ],
-
- "messages": {
-     "echo": {
-         "request": [{"name": "qualified",
-             "type": "org.apache.avro.test.namespace.TestRecord"}],
-         "response": "TestRecord"
-     },
-
-     "error": {
-         "request": [],
-         "response": "null",
-         "errors": ["org.apache.avro.test.namespace.TestError"]
-     }
-
- }
-
-}
-    """, True),
-    ExampleProtocol("""\
-{"namespace": "org.apache.avro.test.namespace",
- "protocol": "TestNamespaceTwo",
-
- "types": [
-     {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
-     {"name": "ReferencedRecord", "type": "record",
-       "namespace": "org.apache.avro.other.namespace",
-       "fields": [ {"name": "foo", "type": "string"} ] },
-     {"name": "TestRecord", "type": "record",
-      "fields": [ {"name": "hash", "type": "org.apache.avro.test.util.MD5"},
-                  {"name": "qualified",
-                    "type": "org.apache.avro.other.namespace.ReferencedRecord"}
-                ]
-     },
-     {"name": "TestError",
-      "type": "error", "fields": [ {"name": "message", "type": "string"} ]
-     }
- ],
-
- "messages": {
-     "echo": {
-         "request": [{"name": "qualified",
-             "type": "org.apache.avro.test.namespace.TestRecord"}],
-         "response": "TestRecord"
-     },
-
-     "error": {
-         "request": [],
-         "response": "null",
-         "errors": ["org.apache.avro.test.namespace.TestError"]
-     }
-
- }
-
-}
-    """, True),
-    ExampleProtocol("""\
-{"namespace": "org.apache.avro.test.namespace",
- "protocol": "TestValidRepeatedName",
-
- "types": [
-     {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
-     {"name": "ReferencedRecord", "type": "record",
-       "namespace": "org.apache.avro.other.namespace",
-       "fields": [ {"name": "foo", "type": "string"} ] },
-     {"name": "ReferencedRecord", "type": "record",
-       "fields": [ {"name": "bar", "type": "double"} ] },
-     {"name": "TestError",
-      "type": "error", "fields": [ {"name": "message", "type": "string"} ]
-     }
- ],
-
- "messages": {
-     "echo": {
-         "request": [{"name": "qualified",
-             "type": "ReferencedRecord"}],
-         "response": "org.apache.avro.other.namespace.ReferencedRecord"
-     },
-
-     "error": {
-         "request": [],
-         "response": "null",
-         "errors": ["org.apache.avro.test.namespace.TestError"]
-     }
-
- }
-
-}
-    """, True),
-    ExampleProtocol("""\
-{"namespace": "org.apache.avro.test.namespace",
- "protocol": "TestInvalidRepeatedName",
-
- "types": [
-     {"name": "org.apache.avro.test.util.MD5", "type": "fixed", "size": 16},
-     {"name": "ReferencedRecord", "type": "record",
-       "fields": [ {"name": "foo", "type": "string"} ] },
-     {"name": "ReferencedRecord", "type": "record",
-       "fields": [ {"name": "bar", "type": "double"} ] },
-     {"name": "TestError",
-      "type": "error", "fields": [ {"name": "message", "type": "string"} ]
-     }
- ],
-
- "messages": {
-     "echo": {
-         "request": [{"name": "qualified",
-             "type": "ReferencedRecord"}],
-         "response": "org.apache.avro.other.namespace.ReferencedRecord"
-     },
-
-     "error": {
-         "request": [],
-         "response": "null",
-         "errors": ["org.apache.avro.test.namespace.TestError"]
-     }
-
- }
-
-}
-    """, False),
-  ExampleProtocol("""\
-{"namespace": "org.apache.avro.test",
- "protocol": "BulkData",
-
- "types": [],
-
- "messages": {
-
-     "read": {
-         "request": [],
-         "response": "bytes"
-     },
-
-     "write": {
-         "request": [ {"name": "data", "type": "bytes"} ],
-         "response": "null"
-     }
-
- }
-
-}
-    """, True),
-  ExampleProtocol("""\
-{
-  "protocol" : "API",
-  "namespace" : "xyz.api",
-  "types" : [ {
-    "type" : "enum",
-    "name" : "Symbology",
-    "namespace" : "xyz.api.product",
-    "symbols" : [ "OPRA", "CUSIP", "ISIN", "SEDOL" ]
-  }, {
-    "type" : "record",
-    "name" : "Symbol",
-    "namespace" : "xyz.api.product",
-    "fields" : [ {
-      "name" : "symbology",
-      "type" : "xyz.api.product.Symbology"
+TEST_API = ExampleProtocol({
+    "namespace": "xyz.api",
+    "protocol": "API",
+    "types": [{
+        "type": "enum",
+        "name": "Symbology",
+        "namespace": "xyz.api.product",
+        "symbols": ["OPRA", "CUSIP", "ISIN", "SEDOL"]
     }, {
-      "name" : "symbol",
-      "type" : "string"
-    } ]
-  }, {
-    "type" : "record",
-    "name" : "MultiSymbol",
-    "namespace" : "xyz.api.product",
-    "fields" : [ {
-      "name" : "symbols",
-      "type" : {
-        "type" : "map",
-        "values" : "xyz.api.product.Symbol"
-      }
-    } ]
-  } ],
-  "messages" : {
-  }
-}
-    """, True),
-]
+        "type": "record",
+        "name": "Symbol",
+        "namespace": "xyz.api.product",
+        "fields": [{
+            "name": "symbology",
+            "type": "xyz.api.product.Symbology"
+        }, {
+            "name": "symbol",
+            "type": "string"
+        }]
+    }, {
+        "type": "record",
+        "name": "MultiSymbol",
+        "namespace": "xyz.api.product",
+        "fields": [{
+            "name": "symbols",
+            "type": {
+                "type": "map",
+                "values": "xyz.api.product.Symbol"
+            }
+        }]
+    }],
+    "messages": {}
+}, True)
 
+EXAMPLES = [HELLO_WORLD,
+            TEST_SIMPLE,
+            TEST_NAMESPACE,
+            TEST_IMPLICIT_NAMESPACE,
+            TEST_NAMESPACE_TWO,
+            TEST_VALID_REPEATED_NAME,
+            TEST_INVALID_REPEATED_NAME,
+            TEST_BULK_DATA,
+            TEST_API]
 VALID_EXAMPLES = [e for e in EXAMPLES if e.valid]
 
 
