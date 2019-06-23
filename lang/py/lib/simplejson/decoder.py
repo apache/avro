9d14de58251397cd54f7d21cf8f55f0e187dd772
Michael A. Smith <michaels@syapse.com>
2019-06-23 14:36:37 -0400

    AVRO-2410: PEP8 W293

--- lang/py/lib/simplejson/decoder.py
+++ lang/py/lib/simplejson/decoder.py
@@ -72,7 +72,7 @@ def py_scanstring(s, end, encoding=None, strict=True, _b=BACKSLASH, _m=STRINGCHU
     Unescapes all valid JSON string escape sequences and raises ValueError
     on attempt to decode an invalid string. If strict is False then literal
     control characters are allowed in the string.
-    
+
     Returns a tuple of the decoded string and the index of the character in s
     after the end quote."""
     if encoding is None:
