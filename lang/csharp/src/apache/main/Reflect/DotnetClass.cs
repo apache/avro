6cf084236850e6534281dec95a0162f1d517289a
pfarry <patrick.farry@pb.com>
2019-07-29 08:46:24 -0700

    Fixed copyright

--- lang/csharp/src/apache/main/Reflect/DotnetClass.cs
+++ lang/csharp/src/apache/main/Reflect/DotnetClass.cs
@@ -1,5 +1,4 @@
-/*  Copyright 2019 Pitney Bowes Inc.
- *
+/*  
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
  * distributed with this work for additional information
