c73c2c6ccc97cb4bfcd22257b2816cc35e2e5c98
Michael A. Smith <michael@smith-li.com>
2019-10-12 17:13:29 -0400

    AVRO-2593: Isolate setUp and tearDown from Real Test

--- lang/py/test/test_tether_word_count.py
+++ lang/py/test/test_tether_word_count.py
@@ -29,6 +29,22 @@ import avro.datafile
 import avro.io
 import avro.schema
 
+_TOPDIR = '@TOPDIR@'
+_AVRO_VERSION = '@AVRO_VERSION@'
+
+_JAR_PATH = os.path.abspath("{}/../java/tools/target/avro-tools-{}.jar".format(
+  _TOPDIR, _AVRO_VERSION))
+
+_CMD = (
+  "java", "-jar", _JAR_PATH, "tether",
+  "-protocol=http",
+  "-program=python",
+  "-exec_args", "-m avro.tether.tether_task_runner word_count_task.WordCountTask",
+  "-in=in",
+  "-out=out",
+  "-outschema=out.avsc",
+)
+
 _INPUT_SCHEMA_STRING = '{"type": "string"}'
 
 # The schema for the output of the mapper and reducer
@@ -64,76 +80,49 @@ def _run(args, env):
     if p is not None and p.returncode is None:
       p.kill()
 
+def _write_lines(lines, fname):
+  """
+  Write the lines to an avro file named fname
+
+  Parameters
+  --------------------------------------------------------
+  lines - list of strings to write
+  fname - the name of the file to write to.
+  """
+  datum_writer = avro.io.DatumWriter(_INPUT_SCHEMA_STRING)
+  wschema = avro.schema.parse(_INPUT_SCHEMA_STRING)
+  with avro.datafile.DataFileWriter(open(fname, 'w'), datum_writer, writers_schema=wschema) as writer:
+    for datum in lines:
+      writer.append(datum)
+
 
 class TestTetherWordCount(unittest.TestCase):
   """unittest for a python tethered map-reduce job."""
 
-  _base_dir = None
-
   def setUp(self):
-    self._base_dir = tempfile.mkdtemp(prefix=__name__ + '_')
+    """Create the necessary tempfiles to run the tethered job."""
+    self._prev_wd = os.getcwd()
+    os.chdir(tempfile.mkdtemp(prefix=__name__ + '_'))
+    os.mkdir('in')
+    _write_lines(_LINES, 'in/lines.avsc')
+    with open('out.avsc', 'w') as out_schema:
+      out_schema.write(_OUTPUT_SCHEMA_STRING)
 
   def tearDown(self):
-    if self._base_dir is not None:
-      shutil.rmtree(self._base_dir)
-
-  def _write_lines(self, lines, fname):
-    """
-    Write the lines to an avro file named fname
-
-    Parameters
-    --------------------------------------------------------
-    lines - list of strings to write
-    fname - the name of the file to write to.
-    """
-    os.makedirs(os.path.dirname(fname))
-    datum_writer = avro.io.DatumWriter(_INPUT_SCHEMA_STRING)
-    wschema = avro.schema.parse(_INPUT_SCHEMA_STRING)
-    with open(fname, 'w') as hf, \
-        avro.datafile.DataFileWriter(hf, datum_writer, writers_schema=wschema) as writer:
-      for datum in lines:
-        writer.append(datum)
-
-  def _tether_tool_command_line(self, inpath, outpath, outschema):
-    return (
-      "java",
-      "-jar",
-      os.path.abspath("@TOPDIR@/../java/tools/target/avro-tools-@AVRO_VERSION@.jar"),
-      "tether",
-      "-in", inpath,
-      "-out", outpath,
-      "-outschema", outschema,
-      "-protocol", "http",
-      "-program", "python",
-      "-exec_args", "-m avro.tether.tether_task_runner word_count_task.WordCountTask",
-    )
+    """Clean up and remove temp files."""
+    tmpdir = os.getcwd()
+    os.chdir(self._prev_wd)
+    # self.assertFalse(os.path.commonpath(self._prev_wd, tmpdir))
+    # shutil.rmtree(tmpdir)
 
   def test_tethered_word_count(self):
     """A tethered map-reduce wordcount job should correctly count words."""
-    base_dir = self._base_dir
-    inpath = os.path.join(base_dir, "in")
-    infile = os.path.join(inpath, "lines.avro")
-    outpath = os.path.join(base_dir, "out")
-    if os.path.exists(base_dir):
-      shutil.rmtree(base_dir)
-    self._write_lines(_LINES, infile)
-    if not(os.path.exists(infile)):
-      self.fail("Missing the input file {0}".format(infile))
-    # write the schema to a temporary file
-    with tempfile.NamedTemporaryFile(mode='w', suffix=".avsc", prefix="wordcount", delete=False) as osfile:
-      osfile.write(_OUTPUT_SCHEMA_STRING)
-    outschema = osfile.name
-
-    if not(os.path.exists(outschema)):
-      self.fail("Missing the schema file")
-
-    args = self._tether_tool_command_line(inpath, outpath, outschema)
-    print("Command:\n\t{0}".format(" ".join(args)))
-    self.assertEqual(0, _run(args, {"PYTHONPATH": _INNER_PYTHON_PATH}))
+    print("Command:")
+    print("\t" + " ".join(_CMD))
+    self.assertEqual(0, _run(_CMD, {"PYTHONPATH": _INNER_PYTHON_PATH}))
 
     # read the output
-    with open(os.path.join(outpath, "part-00000.avro")) as hf, \
-        avro.datafile.DataFileReader(hf, avro.io.DatumReader()) as reader:
+    with avro.datafile.DataFileReader(open("out/part-00000.avro"), avro.io.DatumReader()) as reader:
       for record in reader:
         self.assertEqual(record["value"], _EXPECTED_COUNTS[record["key"]])
 
