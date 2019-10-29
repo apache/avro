2c97b00aa6300f5b453327c4ce75ac2f30921961
timjroberts <tim@timjroberts.com>
2019-10-29 18:59:11 +0000

    update logical Date and Time tests to be local time zone aware

--- lang/csharp/src/apache/test/Util/LogicalTypeTests.cs
+++ lang/csharp/src/apache/test/Util/LogicalTypeTests.cs
@@ -1,20 +1,21 @@
-/**
-* Licensed to the Apache Software Foundation (ASF) under one
-* or more contributor license agreements.  See the NOTICE file
-* distributed with this work for additional information
-* regarding copyright ownership.  The ASF licenses this file
-* to you under the Apache License, Version 2.0 (the
-* "License"); you may not use this file except in compliance
-* with the License.  You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 using System;
 using System.Globalization;
 using Avro.Util;
@@ -25,6 +26,8 @@ namespace Avro.Test
     [TestFixture]
     class LogicalTypeTests
     {
+        private static readonly TimeZoneInfo GmtPlusOneHour = TimeZoneInfo.CreateCustomTimeZone("(GMT+01:00)", new TimeSpan(1, 0, 0), "GMT +1 Hour", "GMT+1");
+
         [TestCase("1234.56")]
         [TestCase("-1234.56")]
         [TestCase("123456789123456789.56")]
@@ -79,7 +82,11 @@ namespace Avro.Test
             var schema = (LogicalSchema)Schema.Parse("{\"type\": \"int\", \"logicalType\": \"date\"}");
 
             var date = DateTime.Parse(s, CultureInfo.GetCultureInfo("en-US").DateTimeFormat, DateTimeStyles.RoundtripKind);
-            date = TimeZoneInfo.ConvertTime(date, TimeZoneInfo.FindSystemTimeZoneById("GMT Standard Time"));
+
+            if (date.Kind == DateTimeKind.Local)
+            {
+                date = TimeZoneInfo.ConvertTime(date, GmtPlusOneHour);
+            }
 
             var avroDate = new Date();
 
@@ -100,6 +107,12 @@ namespace Avro.Test
             var schema = (LogicalSchema)Schema.Parse("{\"type\": \"int\", \"logicalType\": \"date\"}");
 
             var date = DateTime.Parse(s, CultureInfo.GetCultureInfo("en-US").DateTimeFormat, DateTimeStyles.RoundtripKind);
+
+            if (date.Kind == DateTimeKind.Local)
+            {
+                date = TimeZoneInfo.ConvertTime(date, GmtPlusOneHour);
+            }
+
             var expectedDate = DateTime.Parse(e, CultureInfo.GetCultureInfo("en-US").DateTimeFormat, DateTimeStyles.RoundtripKind);
 
             var avroTimestampMilli = new TimestampMillisecond();
