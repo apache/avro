d3ec13db9a2a13ac84f0c1b81aec5927b7f2d29c
Michael A. Smith <michael@smith-li.com>
2019-12-03 18:40:47 -0500

    AVRO-831: Run Tests Normally

--- lang/py/avro/test/test_tether_task_runner.py
+++ lang/py/avro/test/test_tether_task_runner.py
@@ -166,7 +166,7 @@ class TestTetherTaskRunner(unittest.TestCase):
       env={"AVRO_TETHER_OUTPUT_PORT":"{0}".format(parent_port)}
       env["PYTHONPATH"]=':'.join(sys.path)
 
-      runnerproc = subprocess.Popen(["python", avro.tether.tether_task_runner.__file__, "word_count_task.WordCountTask"],env=env)
+      runnerproc = subprocess.Popen(["python", avro.tether.tether_task_runner.__file__, "avro.test.word_count_task.WordCountTask"], env=env)
 
       #possible race condition wait for the process to start
       time.sleep(1)
