652b233e7f33b1c65a578519d3cf7c3c1ae32f4b
Michael Krolikowski <mkroli@users.noreply.github.com>
2019-12-22 22:00:52 +0100

    Negative block lengths are not handled (#100)
    * fixes flavray/avro-rs#69
    
    * tests for array/map decoding
    
    * code formatting

--- src/decode.rs
+++ src/decode.rs
@@ -79,12 +79,16 @@ pub fn decode<R: Read>(schema: &Schema, reader: &mut R) -> Result<Value, Error> 
             let mut items = Vec::new();
 
             loop {
-                let len = decode_len(reader)?;
+                let mut len = zag_i64(reader)?;
                 // arrays are 0-terminated, 0i64 is also encoded as 0 in Avro
                 // reading a length of 0 means the end of the array
                 if len == 0 {
                     break;
+                } else if len < 0 {
+                    let _size = zag_i64(reader)?;
+                    len = -len;
                 }
+                let len = safe_len(len as usize)?;
 
                 items.reserve(len as usize);
                 for _ in 0..len {
@@ -98,12 +102,16 @@ pub fn decode<R: Read>(schema: &Schema, reader: &mut R) -> Result<Value, Error> 
             let mut items = HashMap::new();
 
             loop {
-                let len = decode_len(reader)?;
+                let mut len = zag_i64(reader)?;
                 // maps are 0-terminated, 0i64 is also encoded as 0 in Avro
                 // reading a length of 0 means the end of the map
                 if len == 0 {
                     break;
+                } else if len < 0 {
+                    let _size = zag_i64(reader)?;
+                    len = -len;
                 }
+                let len = safe_len(len as usize)?;
 
                 items.reserve(len as usize);
                 for _ in 0..len {
@@ -152,5 +160,43 @@ pub fn decode<R: Read>(schema: &Schema, reader: &mut R) -> Result<Value, Error> 
                 Err(DecodeError::new("enum symbol not found").into())
             }
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::types::Value::{Array, Bytes, Int, Map};
+
+    #[test]
+    fn test_decode_array_without_size() {
+        let mut input: &[u8] = &[6, 2, 4, 6, 0];
+        let result = decode(&Schema::Array(Box::new(Schema::Int)), &mut input);
+        assert_eq!(Array(vec!(Int(1), Int(2), Int(3))), result.unwrap());
+    }
+
+    #[test]
+    fn test_decode_array_with_size() {
+        let mut input: &[u8] = &[5, 6, 2, 4, 6, 0];
+        let result = decode(&Schema::Array(Box::new(Schema::Int)), &mut input);
+        assert_eq!(Array(vec!(Int(1), Int(2), Int(3))), result.unwrap());
+    }
+
+    #[test]
+    fn test_decode_map_without_size() {
+        let mut input: &[u8] = &[0x02, 0x08, 0x74, 0x65, 0x73, 0x74, 0x02, 0x00];
+        let result = decode(&Schema::Map(Box::new(Schema::Int)), &mut input);
+        let mut expected = HashMap::new();
+        expected.insert(String::from("test"), Int(1));
+        assert_eq!(Map(expected), result.unwrap());
+    }
+
+    #[test]
+    fn test_decode_map_with_size() {
+        let mut input: &[u8] = &[0x01, 0x0C, 0x08, 0x74, 0x65, 0x73, 0x74, 0x02, 0x00];
+        let result = decode(&Schema::Map(Box::new(Schema::Int)), &mut input);
+        let mut expected = HashMap::new();
+        expected.insert(String::from("test"), Int(1));
+        assert_eq!(Map(expected), result.unwrap());
     }
 }
