302f8953dff286e5fc30fc162aacf9111b62413c
Markus Helbig <mhelbig81@googlemail.com>
2020-02-23 08:30:35 +0100

    update to Avro 1.9.2 since https://issues.apache.org/jira/browse/AVRO-2548 has been fixed there

--- src/test/groovy/com/commercehub/gradle/plugin/avro/CustomConversionFunctionalSpec.groovy
+++ src/test/groovy/com/commercehub/gradle/plugin/avro/CustomConversionFunctionalSpec.groovy
@@ -15,7 +15,10 @@
  */
 package com.commercehub.gradle.plugin.avro
 
+import spock.lang.Requires
+
 import static org.gradle.testkit.runner.TaskOutcome.SUCCESS
+import static org.gradle.util.GradleVersion.version
 
 class CustomConversionFunctionalSpec extends FunctionalSpec {
     def "setup"() {
@@ -30,6 +33,43 @@ class CustomConversionFunctionalSpec extends FunctionalSpec {
             "com/commercehub/gradle/plugin/avro/test/custom/TimeZoneLogicalType.java")
         copyFile("src/test/java", destDir,
             "com/commercehub/gradle/plugin/avro/test/custom/TimeZoneLogicalTypeFactory.java")
+    }
+
+    @Requires({ version(avroVersion).compareTo(version("1.9.2")) >= 0 })
+    def "can use a custom conversion when generating java from a schema with stringType = \"String\""() {
+        // since Avro 1.9.2 https://issues.apache.org/jira/browse/AVRO-2548 is fixed
+        given:
+        copyResource("customConversion.avsc", avroDir)
+        buildFile << """
+            import com.commercehub.gradle.plugin.avro.test.custom.*
+            avro {
+                stringType = "String"
+                logicalTypeFactory("timezone", TimeZoneLogicalTypeFactory)
+                customConversion(TimeZoneConversion)
+            }
+        """
+        testProjectDir.newFolder("buildSrc")
+        testProjectDir.newFile("buildSrc/build.gradle") << """
+            repositories {
+                jcenter()
+            }
+            dependencies {
+                compile "org.apache.avro:avro:${avroVersion}"
+            }
+        """
+        copyCustomConversion("buildSrc/src/main/java")
+        copyCustomConversion("src/main/java")
+
+        when:
+        def result = run()
+
+        then:
+        taskInfoAbsent || result.task(":generateAvroJava").outcome == SUCCESS
+        taskInfoAbsent || result.task(":compileJava").outcome == SUCCESS
+        projectFile(buildOutputClassPath("test/Event.class")).file
+        def javaSource = projectFile("build/generated-main-avro-java/test/Event.java").text
+        javaSource.contains("java.time.Instant start;")
+        javaSource.contains("java.util.TimeZone timezone;")
     }
 
     def "can use a custom conversion when generating java from a schema"() {
