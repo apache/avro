d292b92f137561d0af0e82b90df8cb51b040963d
Siad Ardroumli <siad.ardroumli@gmail.com>
2020-05-09 16:18:11 +0200

    Add AvroNotImplementedException

--- lang/php/lib/avro.php
+++ lang/php/lib/avro.php
@@ -33,6 +33,12 @@
 class AvroException extends Exception {}
 
 /**
+ * Avro "not implemented method" exception.
+ * @package Avro
+ */
+class AvroNotImplementedException extends AvroException {}
+
+/**
  * Library-level class for PHP Avro port.
  *
  * Contains library details such as version number and platform checks.
