2502a648c83bcdc3a00e66d476293f86010b1e95
Kyoungha Min <kyoungha@molocoads.com>
2020-09-30 08:27:07 -0700

    AVRO-2905: Fix Utf8 hash cache (#955)
    * AVRO-2905: Fix Utf8 hash cache
    
    * AVRO-2905: Reflect Comment, add length check

--- lang/java/avro/src/test/java/org/apache/avro/util/TestUtf8.java
+++ lang/java/avro/src/test/java/org/apache/avro/util/TestUtf8.java
@@ -17,6 +17,7 @@
  */
 package org.apache.avro.util;
 
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertSame;
 import static org.junit.Assert.assertEquals;
 
@@ -51,13 +52,42 @@ public class TestUtf8 {
 
   @Test
   public void testHashCodeReused() {
+    assertEquals(97, new Utf8("a").hashCode());
+    assertEquals(3904, new Utf8("zz").hashCode());
+    assertEquals(122, new Utf8("z").hashCode());
+    assertEquals(99162322, new Utf8("hello").hashCode());
+    assertEquals(3198781, new Utf8("hell").hashCode());
+
     Utf8 u = new Utf8("a");
     assertEquals(97, u.hashCode());
+    assertEquals(97, u.hashCode());
 
+    u.set("a");
+    assertEquals(97, u.hashCode());
+
+    u.setByteLength(1);
+    assertEquals(97, u.hashCode());
     u.setByteLength(2);
-    u.set("zz");
+    assertNotEquals(97, u.hashCode());
 
-    assertEquals(97121, u.hashCode());
-    assertEquals(97121, u.hashCode());
+    u.set("zz");
+    assertEquals(3904, u.hashCode());
+    u.setByteLength(1);
+    assertEquals(122, u.hashCode());
+
+    u.set("hello");
+    assertEquals(99162322, u.hashCode());
+    u.setByteLength(4);
+    assertEquals(3198781, u.hashCode());
+
+    u.set(new Utf8("zz"));
+    assertEquals(3904, u.hashCode());
+    u.setByteLength(1);
+    assertEquals(122, u.hashCode());
+
+    u.set(new Utf8("hello"));
+    assertEquals(99162322, u.hashCode());
+    u.setByteLength(4);
+    assertEquals(3198781, u.hashCode());
   }
 }
