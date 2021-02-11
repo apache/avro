791406e22a4a5274ababe0b9ffaaa113bfd31c77
jgonyo-snag <jgonyo@snagajob.com>
2021-02-11 10:30:21 -0500

    AVRO-3038: enable support for nullable array/enum

--- lang/csharp/src/apache/main/Reflect/ClassCache.cs
+++ lang/csharp/src/apache/main/Reflect/ClassCache.cs
@@ -19,6 +19,7 @@
 using System;
 using System.Collections;
 using System.Collections.Concurrent;
+using System.Collections.Generic;
 
 namespace Avro.Reflect
 {
@@ -251,11 +252,31 @@ namespace Avro.Reflect
                     LoadClassCache(objType.GenericTypeArguments[1], ms.ValueSchema);
                     break;
                 case NamedSchema ns:
-                    EnumCache.AddEnumNameMapItem(ns, objType);
+                    // add the non-nullable type to the the enum map, not the nullable one
+                    if (objType.IsGenericType && objType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                    {
+                        EnumCache.AddEnumNameMapItem(ns, objType.GenericTypeArguments[0]);
+                    }
+                    else
+                    {
+                        EnumCache.AddEnumNameMapItem(ns, objType);
+                    }
+
                     break;
                 case UnionSchema us:
-                    if (us.Schemas.Count == 2 && (us.Schemas[0].Tag == Schema.Type.Null || us.Schemas[1].Tag == Schema.Type.Null) && objType.IsClass)
+                    if (us.Schemas.Count == 2 &&
+                        (us.Schemas[0].Tag == Schema.Type.Null || us.Schemas[1].Tag == Schema.Type.Null) &&
+                        (objType.IsClass
+                         || (objType.IsGenericType
+                             && (
+                                 objType.GetGenericTypeDefinition() == typeof(Nullable<>)
+                                 || objType.GetGenericTypeDefinition() == typeof(IList<>)
+                                 )
+                             )
+                         )
+                        )
                     {
+                        // this is a Nullable union where the type is a class, Nullable primitive, or an IList<T>
                         // in this case objType will match the non null type in the union
                         foreach (var o in us.Schemas)
                         {
@@ -264,7 +285,6 @@ namespace Avro.Reflect
                                 LoadClassCache(objType, o);
                             }
                         }
-
                     }
                     else
                     {
