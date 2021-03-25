257293190f23d02c773bc883997746f8ef083a09
Subhash Bhushan <subhash.bhushan@gmail.com>
2021-03-25 10:42:05 -0700

    AVRO-2720 Add test for record type exception

--- lang/py/avro/test/test_io.py
+++ lang/py/avro/test/test_io.py
@@ -413,6 +413,18 @@ class TestIO(unittest.TestCase):
             write_datum(datum_to_write, writers_schema)
         assert str(exc.exception) == 'The datum "5.0" provided for "foo" is not an example of the schema "long"'
 
+    def test_type_exception_record(self):
+        writers_schema = avro.schema.parse(json.dumps({
+            "type": "record", "name": "Test",
+            "fields": [
+                {"name": "foo", "type": "long"}]}))
+        datum_to_write = ('foo', 5.0)
+        pretty_expected = json.dumps(json.loads(str(writers_schema)), indent=2)
+        with self.assertRaises(avro.errors.AvroTypeException) as exc:
+            write_datum(datum_to_write, writers_schema)
+        assert str(exc.exception) == \
+            f"""The datum "(\'foo\', 5.0)" provided for "Test" is not an example of the schema {pretty_expected}"""
+
 
 if __name__ == '__main__':
     unittest.main()
