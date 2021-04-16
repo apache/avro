b47eda9d130f09847d5c666ce8b057196963b578
Tim Perkins <tjwp@users.noreply.github.com>
2021-04-16 08:02:31 -0400

    AVRO-3108: Drop support for Ruby 2.5 (#1186)
    * AVRO-3108: Drop support for Ruby 2.5
    
    * Additional updates for Ruby 2.6
    
    * User Ruby 2.6 in Docker build
    
    * Don't assume rbenv in CI

--- lang/ruby/build.sh
+++ lang/ruby/build.sh
@@ -22,10 +22,13 @@ cd "$(dirname "$0")"
 
 # maintain our gems here
 export GEM_HOME="$PWD/.gem/"
-export PATH="$GEM_HOME/bin:$PATH"
+export PATH="/usr/local/rbenv/shims:$GEM_HOME/bin:$PATH"
 
 # bootstrap bundler
 gem install --no-document -v 1.17.3 bundler
+
+# rbenv is used by the Dockerfile but not the Github action in CI
+rbenv rehash || echo "Not using rbenv"
 bundle install
 
 for target in "$@"
