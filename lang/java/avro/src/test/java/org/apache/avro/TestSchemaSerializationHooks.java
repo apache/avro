ee4fd9f4bd95d814313777002e016748b1c86bca
Zoltan Farkas <zolyfarkas@yahoo.com>
2021-05-11 07:09:49 -0400

    [fix] lint issues

--- lang/java/avro/src/test/java/org/apache/avro/TestSchemaSerializationHooks.java
+++ lang/java/avro/src/test/java/org/apache/avro/TestSchemaSerializationHooks.java
@@ -1,5 +1,21 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.apache.avro;
-
 
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonNode;
@@ -13,13 +29,9 @@ public class TestSchemaSerializationHooks {
 
   @Test
   public void testSchemaRefResolver() throws IOException {
-    Schema recSchema = SchemaBuilder.record("TestRecord")
-            .prop("id", "testId")
-            .fields()
-            .nullableLong("number", 0L)
-            .endRecord();
-    Schema schema = SchemaBuilder.array()
-            .items(recSchema);
+    Schema recSchema = SchemaBuilder.record("TestRecord").prop("id", "testId").fields().nullableLong("number", 0L)
+        .endRecord();
+    Schema schema = SchemaBuilder.array().items(recSchema);
     StringWriter stringWriter = new StringWriter();
     JsonGenerator jgen = Schema.FACTORY.createGenerator(stringWriter);
     schema.toJson(new TestResolver(recSchema), jgen);
@@ -43,8 +55,6 @@ public class TestSchemaSerializationHooks {
       super(space);
       this.recSchema = recSchema;
     }
-
-
 
     public Schema customRead(Function<String, JsonNode> object) {
       JsonNode node = object.apply("$ref");
