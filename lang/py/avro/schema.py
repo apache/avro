6ad00389c9ac2ced43dabbc38b58a626c8a4722b
Subhash Bhushan <subhash.bhushan@gmail.com>
2021-07-08 06:20:24 -0700

    Freeze Supported Algorithms Set
    This commit addresses review comments and freezes the supported
    fingerprinting algorithms set.

--- lang/py/avro/schema.py
+++ lang/py/avro/schema.py
@@ -179,9 +179,23 @@ class FingerprintMixin:
     _EMPTY64 = 0xC15D213AA4D7A795
 
     # All algorithms guaranteed by hashlib are supported
-    SUPPORTED_ALGORITHMS = set(hashlib.algorithms_guaranteed)
     # Additionally, we provide a custom implementation of 64-bit Rabin fingerprint
-    SUPPORTED_ALGORITHMS.update({"CRC-64-AVRO"})
+    # Supported Algorithms are:
+    #     - 'blake2b', 
+    #     - 'blake2s',
+    #     - 'md5',
+    #     - 'sha1',
+    #     - 'sha224',
+    #     - 'sha256',
+    #     - 'sha384',
+    #     - 'sha3_224',
+    #     - 'sha3_256',
+    #     - 'sha3_384',
+    #     - 'sha3_512',
+    #     - 'sha512',
+    #     - 'shake_128',
+    #     - 'shake_256'
+    SUPPORTED_ALGORITHMS = frozenset(hashlib.algorithms_guaranteed.union({"CRC-64-AVRO"}))
 
     def fingerprint(self, algorithm="CRC-64-AVRO"):
         """
