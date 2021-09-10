5ad692bb5f4c29433a7a737ff3b01d24315aad52
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2021-09-10 16:22:27 +0300

    AVRO-3200 Fix some new lint errors
    Reported by a the latest version of cargo-clippy

--- lang/rust/benches/serde.rs
+++ lang/rust/benches/serde.rs
@@ -176,7 +176,7 @@ fn make_records(record: Value, count: usize) -> Vec<Value> {
 }
 
 fn write(schema: &Schema, records: &[Value]) -> Vec<u8> {
-    let mut writer = Writer::new(&schema, Vec::new());
+    let mut writer = Writer::new(schema, Vec::new());
     writer.extend_from_slice(records).unwrap();
     writer.into_inner().unwrap()
 }
