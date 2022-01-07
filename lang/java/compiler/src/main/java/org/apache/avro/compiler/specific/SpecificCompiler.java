b47ac8e30e0d3d94d58bbad2e3162372064825f6
Kyle Carter <kcarter@mastercontrol.com>
2022-01-07 16:14:29 -0700

    AVRO-3305: Only mangle type identifier keywords when types
    Instead of treating all Java keywords identically, only mangles what
    the JLS terms as "contextual keywords" when used in the context they
    are reserved. Specifically, only mangle reserved type identifiers
    when used as type identifiers.

--- lang/java/compiler/src/main/java/org/apache/avro/compiler/specific/SpecificCompiler.java
+++ lang/java/compiler/src/main/java/org/apache/avro/compiler/specific/SpecificCompiler.java
@@ -143,6 +143,15 @@ public class SpecificCompiler {
     ACCESSOR_MUTATOR_RESERVED_WORDS.addAll(RESERVED_WORDS);
   }
 
+  /* Reserved words for type identifiers */
+  private static final Set<String> TYPE_IDENTIFIER_RESERVED_WORDS = new HashSet<>(
+      Arrays.asList("var", "yield", "record"));
+
+  static {
+    // Add reserved words to type identifier reserved words
+    TYPE_IDENTIFIER_RESERVED_WORDS.addAll(RESERVED_WORDS);
+  }
+
   /* Reserved words for error types */
   private static final Set<String> ERROR_RESERVED_WORDS = new HashSet<>(Arrays.asList("message", "cause"));
 
@@ -558,7 +567,7 @@ public class SpecificCompiler {
     String out = renderTemplate(templateDir + "protocol.vm", context);
 
     OutputFile outputFile = new OutputFile();
-    String mangledName = mangle(protocol.getName());
+    String mangledName = mangleTypeIdentifier(protocol.getName());
     outputFile.path = makePath(mangledName, mangle(protocol.getNamespace()));
     outputFile.contents = out;
     outputFile.outputCharacterEncoding = outputCharacterEncoding;
@@ -630,7 +639,7 @@ public class SpecificCompiler {
     }
 
     OutputFile outputFile = new OutputFile();
-    String name = mangle(schema.getName());
+    String name = mangleTypeIdentifier(schema.getName());
     outputFile.path = makePath(name, mangle(schema.getNamespace()));
     outputFile.contents = output;
     outputFile.outputCharacterEncoding = outputCharacterEncoding;
@@ -796,7 +805,7 @@ public class SpecificCompiler {
     case RECORD:
     case ENUM:
     case FIXED:
-      return mangle(schema.getFullName());
+      return mangleFullyQualified(schema.getFullName());
     case ARRAY:
       return "java.util.List<" + javaType(schema.getElementType()) + ">";
     case MAP:
@@ -825,6 +834,19 @@ public class SpecificCompiler {
       return "java.lang.Void";
     default:
       throw new RuntimeException("Unknown type: " + schema);
+    }
+  }
+
+  private String mangleFullyQualified(String fullName) {
+    int lastDot = fullName.lastIndexOf('.');
+
+    if (lastDot < 0) {
+      return mangleTypeIdentifier(fullName);
+    } else {
+      String namespace = fullName.substring(0, lastDot);
+      String typeName = fullName.substring(lastDot + 1);
+
+      return mangle(namespace) + "." + mangleTypeIdentifier(typeName);
     }
   }
 
@@ -1058,6 +1080,22 @@ public class SpecificCompiler {
    */
   public static String mangle(String word, boolean isError) {
     return mangle(word, isError ? ERROR_RESERVED_WORDS : RESERVED_WORDS);
+  }
+
+  /**
+   * Utility for template use. Adds a dollar sign to reserved words in type
+   * identifiers.
+   */
+  public static String mangleTypeIdentifier(String word) {
+    return mangleTypeIdentifier(word, false);
+  }
+
+  /**
+   * Utility for template use. Adds a dollar sign to reserved words in type
+   * identifiers.
+   */
+  public static String mangleTypeIdentifier(String word, boolean isError) {
+    return mangle(word, isError ? ERROR_RESERVED_WORDS : TYPE_IDENTIFIER_RESERVED_WORDS);
   }
 
   /**
