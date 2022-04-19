30a5c2ee75249778e5a7f2584ca1b47523c3253c
Zoltan Csizmadia <zcsizmadia@gmail.com>
2022-04-19 13:28:33 -0500

    AVRO-3841: Try exact schema match first in union type (#1635)
    * Try exact schema match
    
    * Fix formatting
    
    * Add tests for exception
    
    Co-authored-by: Zoltan Csizmadia <CsizmadiaZ@valassis.com>
    (cherry picked from commit a852aa433111499f0479a8efd8149e7dcfb1b77a)

--- lang/csharp/src/apache/main/Schema/UnionSchema.cs
+++ lang/csharp/src/apache/main/Schema/UnionSchema.cs
@@ -127,8 +127,21 @@ namespace Avro
         {
             if (s is UnionSchema) throw new AvroException("Cannot find a match against union schema");
             // Try exact match.
-            //for (int i = 0; i < Count; i++) if (Schemas[i].Equals(s)) return i; // removed this for performance's sake
-            for (int i = 0; i < Count; i++) if (Schemas[i].CanRead(s)) return i;
+            // CanRead might find a compatible schema which can read. e.g. double and long
+            for (int i = 0; i < Count; i++)
+            {
+                if (Schemas[i].Equals(s))
+                {
+                    return i;
+                }
+            }
+            for (int i = 0; i < Count; i++)
+            {
+                if (Schemas[i].CanRead(s))
+                {
+                    return i;
+                }
+            }
             return -1;
         }
 
