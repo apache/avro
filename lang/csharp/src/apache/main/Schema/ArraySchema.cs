83dc7e80dbb90bdc59715e6c4639a574664b811a
Kyle Schoonover <kyle@minmaxcorp.com>
2022-04-21 14:27:19 -0700

    Fix additional spacing

--- lang/csharp/src/apache/main/Schema/ArraySchema.cs
+++ lang/csharp/src/apache/main/Schema/ArraySchema.cs
@@ -15,8 +15,8 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 using System;
-using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 
 namespace Avro
--- lang/csharp/src/apache/main/Schema/Field.cs
+++ lang/csharp/src/apache/main/Schema/Field.cs
@@ -15,10 +15,11 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 using System;
 using System.Collections.Generic;
-using Newtonsoft.Json.Linq;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace Avro
 {
