5195df853a7e86b1360c5e1bc189908999a7f45f
Ryan Skraba <ryan@skraba.com>
2022-07-01 18:59:13 +0200

    Fix typo with symbolic link directory

--- build.sh
+++ build.sh
@@ -184,7 +184,7 @@ do
       # If it was a SNAPSHOT, it was lowercased during the build.
       cp -R build/staging-web/public/docs/"${VERSION,,}"/* "build/$DOC_DIR/"
       cp -R "build/$DOC_DIR/api" build/staging-web/public/docs/"${VERSION,,}"/
-      ( cd "${VERSION,,}"; ln -s "${VERSION,,}" current )
+      ( cd build/staging-web/public/docs/; ln -s "${VERSION,,}" current )
       # add LICENSE and NOTICE for docs
       mkdir -p "build/$DOC_DIR"
       cp doc/LICENSE "build/$DOC_DIR"
