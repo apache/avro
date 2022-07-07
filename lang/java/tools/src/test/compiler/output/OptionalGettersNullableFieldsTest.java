4d82fb5dd8ea28594248cc1ac44b8519781f66dd
clesaec <51320496+clesaec@users.noreply.github.com>
2022-07-08 00:16:28 +0200

    AVRO-2774: Generate @Overrides annotations (#1755)
    * AVRO-2774 : add override and test it
    
    * AVRO-2774 : remove sun deps for java17

--- lang/java/tools/src/test/compiler/output/OptionalGettersNullableFieldsTest.java
+++ lang/java/tools/src/test/compiler/output/OptionalGettersNullableFieldsTest.java
@@ -98,9 +98,14 @@ public class OptionalGettersNullableFieldsTest extends org.apache.avro.specific.
     this.nullable_favorite_number = nullable_favorite_number;
   }
 
+  @Override
   public org.apache.avro.specific.SpecificData getSpecificData() { return MODEL$; }
+
+  @Override
   public org.apache.avro.Schema getSchema() { return SCHEMA$; }
+
   // Used by DatumWriter.  Applications should not call.
+  @Override
   public java.lang.Object get(int field$) {
     switch (field$) {
     case 0: return name;
@@ -112,6 +117,7 @@ public class OptionalGettersNullableFieldsTest extends org.apache.avro.specific.
   }
 
   // Used by DatumReader.  Applications should not call.
+  @Override
   @SuppressWarnings(value="unchecked")
   public void put(int field$, java.lang.Object value$) {
     switch (field$) {
