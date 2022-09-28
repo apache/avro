8640436de52ed25ccb0dbfb125cc3f142e968889
Martin Grigorov <martin-g@users.noreply.github.com>
2022-09-28 11:35:34 +0300

    AVRO-3630: [Rust] Make it possible to extend pre-existing Avro bytes (#1888)
    * AVRO-3630: [Rust] Make it possible to extend pre-existing Avro bytes
    
    Make it possible to pass a block marker to Writer, so that it could
    append to pre-existing bytes (i.e. bytes created by another Writer)
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    * AVRO-3630: Change Writer#marker from Vec<u8> to [u8; 16]
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>
    
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- lang/rust/avro/src/writer.rs
+++ lang/rust/avro/src/writer.rs
@@ -47,8 +47,8 @@ pub struct Writer<'a, W> {
     serializer: Serializer,
     #[builder(default = 0, setter(skip))]
     num_values: usize,
-    #[builder(default = generate_sync_marker(), setter(skip))]
-    marker: Vec<u8>,
+    #[builder(default = generate_sync_marker())]
+    marker: [u8; 16],
     #[builder(default = false, setter(skip))]
     has_header: bool,
     #[builder(default)]
@@ -60,9 +60,7 @@ impl<'a, W: Write> Writer<'a, W> {
     /// to.
     /// No compression `Codec` will be used.
     pub fn new(schema: &'a Schema, writer: W) -> Self {
-        let mut w = Self::builder().schema(schema).writer(writer).build();
-        w.resolved_schema = ResolvedSchema::try_from(schema).ok();
-        w
+        Writer::with_codec(schema, writer, Codec::Null)
     }
 
     /// Creates a `Writer` with a specific `Codec` given a `Schema` and something implementing the
@@ -73,6 +71,32 @@ impl<'a, W: Write> Writer<'a, W> {
             .writer(writer)
             .codec(codec)
             .build();
+        w.resolved_schema = ResolvedSchema::try_from(schema).ok();
+        w
+    }
+
+    /// Creates a `Writer` that will append values to already populated
+    /// `std::io::Write` using the provided `marker`
+    /// No compression `Codec` will be used.
+    pub fn append_to(schema: &'a Schema, writer: W, marker: [u8; 16]) -> Self {
+        Writer::append_to_with_codec(schema, writer, Codec::Null, marker)
+    }
+
+    /// Creates a `Writer` that will append values to already populated
+    /// `std::io::Write` using the provided `marker`
+    pub fn append_to_with_codec(
+        schema: &'a Schema,
+        writer: W,
+        codec: Codec,
+        marker: [u8; 16],
+    ) -> Self {
+        let mut w = Self::builder()
+            .schema(schema)
+            .writer(writer)
+            .codec(codec)
+            .marker(marker)
+            .build();
+        w.has_header = true;
         w.resolved_schema = ResolvedSchema::try_from(schema).ok();
         w
     }
@@ -514,16 +538,24 @@ pub fn to_avro_datum<T: Into<Value>>(schema: &Schema, value: T) -> AvroResult<Ve
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-fn generate_sync_marker() -> Vec<u8> {
-    std::iter::repeat_with(rand::random).take(16).collect()
+fn generate_sync_marker() -> [u8; 16] {
+    let mut marker = [0_u8; 16];
+    std::iter::repeat_with(rand::random)
+        .take(16)
+        .enumerate()
+        .for_each(|(i, n)| marker[i] = n);
+    marker
 }
 
 #[cfg(target_arch = "wasm32")]
-fn generate_sync_marker() -> Vec<u8> {
+fn generate_sync_marker() -> [u8; 16] {
+    let mut marker = [0_u8; 16];
     std::iter::repeat_with(quad_rand::rand)
         .take(4)
         .flat_map(|i| i.to_be_bytes())
-        .collect()
+        .enumerate()
+        .for_each(|(i, n)| marker[i] = n);
+    marker
 }
 
 #[cfg(test)]
