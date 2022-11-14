1f7bfc44703b7b4c193efb1a2f44e1ce432a6e0c
Christophe Le Saec <clesaec@talend.com>
2022-11-14 12:41:41 +0100

    avro-3527: GenericData, extract hashcode logic in inner class

--- lang/java/avro/src/main/java/org/apache/avro/generic/GenericData.java
+++ lang/java/avro/src/main/java/org/apache/avro/generic/GenericData.java
@@ -35,7 +35,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.WeakHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import org.apache.avro.AvroMissingFieldException;
 import org.apache.avro.AvroRuntimeException;
@@ -1094,56 +1093,62 @@ public class GenericData {
    * {@link #compare(Object,Object,Schema)}.
    */
   public int hashCode(Object o, Schema s) {
-    return this.hashCode(o, s, new AtomicInteger(10));
+    HashCodeCalculator calculator = new HashCodeCalculator();
+    return calculator.hashCode(o, s);
   }
 
-  /**
-   * Compute a hash code according to a schema, consistent with
-   * {@link #compare(Object,Object,Schema)}.
-   */
-  private int hashCode(Object o, Schema s, AtomicInteger counter) {
-    if (o == null || counter.get() == 0)
-      return 0; // incomplete datum
-    int hashCode = 1;
-    switch (s.getType()) {
-    case RECORD:
-      for (Field f : s.getFields()) {
-        int value = counter.decrementAndGet();
-        if (value == 0) {
-          return hashCode;
+  class HashCodeCalculator {
+    private int counter = 10;
+
+    private int currentHashCode = 1;
+
+    public int hashCode(Object o, Schema s) {
+      if (o == null)
+        return 0; // incomplete datum
+
+      switch (s.getType()) {
+      case RECORD:
+        for (Field f : s.getFields()) {
+          if (this.shouldStop()) {
+            return this.currentHashCode;
+          }
+          if (f.order() == Field.Order.IGNORE)
+            continue;
+          Object fieldValue = ((IndexedRecord) o).get(f.pos());
+          this.currentHashCode = this.hashCodeAdd(fieldValue, f.schema());
         }
-        if (f.order() == Field.Order.IGNORE)
-          continue;
-        hashCode = hashCodeAdd(hashCode, getField(o, f.name(), f.pos()), f.schema(), counter);
-      }
-      return hashCode;
-    case ARRAY:
-      Collection<?> a = (Collection<?>) o;
-      Schema elementType = s.getElementType();
-      for (Object e : a) {
-        int value = counter.decrementAndGet();
-        if (value == 0) {
-          return hashCode;
+        return currentHashCode;
+      case ARRAY:
+        Collection<?> a = (Collection<?>) o;
+        Schema elementType = s.getElementType();
+        for (Object e : a) {
+          if (this.shouldStop()) {
+            return currentHashCode;
+          }
+          currentHashCode = this.hashCodeAdd(e, elementType);
         }
-        hashCode = hashCodeAdd(hashCode, e, elementType, counter);
+        return currentHashCode;
+      case UNION:
+        return hashCode(o, s.getTypes().get(GenericData.this.resolveUnion(s, o)));
+      case ENUM:
+        return s.getEnumOrdinal(o.toString());
+      case NULL:
+        return 0;
+      case STRING:
+        return (o instanceof Utf8 ? o : new Utf8(o.toString())).hashCode();
+      default:
+        return o.hashCode();
       }
-      return hashCode;
-    case UNION:
-      return hashCode(o, s.getTypes().get(resolveUnion(s, o)));
-    case ENUM:
-      return s.getEnumOrdinal(o.toString());
-    case NULL:
-      return 0;
-    case STRING:
-      return (o instanceof Utf8 ? o : new Utf8(o.toString())).hashCode();
-    default:
-      return o.hashCode();
     }
-  }
 
-  /** Add the hash code for an object into an accumulated hash code. */
-  protected int hashCodeAdd(int hashCode, Object o, Schema s, AtomicInteger counter) {
-    return 31 * hashCode + hashCode(o, s, counter);
+    /** Add the hash code for an object into an accumulated hash code. */
+    protected int hashCodeAdd(Object o, Schema s) {
+      return 31 * this.currentHashCode + hashCode(o, s);
+    }
+
+    private boolean shouldStop() {
+      return --counter <= 0;
+    }
   }
 
   /**
