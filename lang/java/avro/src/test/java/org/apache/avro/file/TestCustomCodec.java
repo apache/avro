64f5a430b28dcd0f39b7489b93e1accdf5effea0
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2023-04-11 16:02:18 +0300

    AVRO-3653: Try to debug the issue with Snappy code in Java
    Signed-off-by: Martin Tzvetanov Grigorov <mgrigorov@apache.org>

--- lang/java/avro/src/test/java/org/apache/avro/file/TestCustomCodec.java
+++ lang/java/avro/src/test/java/org/apache/avro/file/TestCustomCodec.java
@@ -33,10 +33,13 @@ public class TestCustomCodec {
   @Test
   void customCodec() {
     CustomCodec customCodec = new CustomCodec();
-    Codec snappyCodec = new SnappyCodec.Option().createInstance();
-    assertEquals(customCodec, new CustomCodec());
-    assertNotEquals(customCodec, snappyCodec);
-
+    try {
+      Codec snappyCodec = new SnappyCodec.Option().createInstance();
+      assertEquals(customCodec, new CustomCodec());
+      assertNotEquals(customCodec, snappyCodec);
+    } catch (Throwable e) {
+      throw new RuntimeException(e);
+    }
     String testString = "Testing 123";
     ByteBuffer original = ByteBuffer.allocate(testString.getBytes(UTF_8).length);
     original.put(testString.getBytes(UTF_8));
