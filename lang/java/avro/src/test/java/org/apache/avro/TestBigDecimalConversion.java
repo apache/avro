79ffe269cd977d8d5ee1327f5fe9928420d6d435
Christophe Le Saec <clesaec@talend.com>
2023-06-15 13:56:18 +0200

    AVRO-3779: fix for java17

--- lang/java/avro/src/test/java/org/apache/avro/TestBigDecimalConversion.java
+++ lang/java/avro/src/test/java/org/apache/avro/TestBigDecimalConversion.java
@@ -19,6 +19,7 @@
 package org.apache.avro;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
