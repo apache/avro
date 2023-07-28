5ee8f11057a886896e5736ea33061d06fd34b5f4
Rik Heijdens <r.heijdens@lithic.com>
2023-07-28 13:57:01 +0200

    AVRO-3786: Revert change to UnionSchema::find_schema_with_known_schemata

--- lang/rust/avro/src/schema.rs
+++ lang/rust/avro/src/schema.rs
@@ -826,13 +826,9 @@ impl UnionSchema {
                 let namespace = &schema.namespace().or_else(|| enclosing_namespace.clone());
 
                 // Attempt to validate the value in order to ensure we've selected the right schema.
-                match value.validate_internal(schema, &collected_names, namespace, true) {
-                    None => true,
-                    Some(err) => {
-                        println!("Validation failed: {:?}", err);
-                        false
-                    }
-                }
+                value
+                    .validate_internal(schema, &collected_names, namespace, true)
+                    .is_none()
             })
         }
     }
