af974cdeebfa154b1e317dbc953c129ff1ba202e
Roman Mitasov <roman.mitasov@internet.ru>
2024-03-06 01:26:02 +0500

    Fix NPE in Protocol#equals

--- lang/java/avro/src/main/java/org/apache/avro/Protocol.java
+++ lang/java/avro/src/main/java/org/apache/avro/Protocol.java
@@ -39,6 +39,7 @@ import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 
 /**
@@ -390,13 +391,14 @@ public class Protocol extends JsonProperties {
     if (!(o instanceof Protocol))
       return false;
     Protocol that = (Protocol) o;
-    return this.name.equals(that.name) && this.namespace.equals(that.namespace) && this.types.equals(that.types)
-        && this.messages.equals(that.messages) && this.propsEqual(that);
+    return Objects.equals(this.name, that.name) && Objects.equals(this.namespace, that.namespace)
+        && Objects.equals(this.types, that.types) && Objects.equals(this.messages, that.messages)
+        && this.propsEqual(that);
   }
 
   @Override
   public int hashCode() {
-    return name.hashCode() + namespace.hashCode() + types.hashCode() + messages.hashCode() + propsHashCode();
+    return name.hashCode() + Objects.hash(namespace) + types.hashCode() + messages.hashCode() + propsHashCode();
   }
 
   /** Render this as <a href="https://json.org/">JSON</a>. */
