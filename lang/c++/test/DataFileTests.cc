apache/avro

commit 380184cfefb563c32067ac2c93b1682d63e0ccb3

Minor: Remove `api` directory from cpp dist (#3042)
* Minor: Remove `api` directory from cpp dist

```
+ ./build.sh dist
-- Version: 10.2.1
-- Build type: Release
Enabled snappy codec
-- Configuring done
-- Generating done
-- Build files have been written to: /home/fokko.driesprong/avro/lang/c++/build
-- Version: 10.2.1
-- Build type: Release
Enabled snappy codec
-- Configuring done
-- Generating done
-- Build files have been written to: /home/fokko.driesprong/avro/lang/c++/build
cp: cannot stat 'api': No such file or directory
``

* Thanks Martin

* Update lang/c++/build.sh

Co-authored-by: Martin Grigorov <martin-g@users.noreply.github.com>

---------

Co-authored-by: Martin Grigorov <martin-g@users.noreply.github.com>

lang/c++/build.sh
@@ -58,7 +58,7 @@ function do_doc() {
 function do_dist() {
   rm -rf $BUILD_CPP/
   mkdir -p $BUILD_CPP
-  cp -r api AUTHORS build.sh CMakeLists.txt ChangeLog \
+  cp -r include AUTHORS build.sh CMakeLists.txt ChangeLog \
     LICENSE NOTICE impl jsonschemas NEWS README test examples \
     $BUILD_CPP
   find $BUILD_CPP -name '.svn' | xargs rm -rf

commit 3936cac146d5f7fddba57de6534f029976bf925c

[AVRO-3710] [C++] Permit DataFile use without stream ownership
As requested by AVRO-3710, this makes it possible to create DataFile
reader and writer instances which do not take exclusive ownership of
their underlying stream. This makes it more convenient to use data files
without effectively being forced to write to a file.

This also fixes an issue with the build format and cppcheck steps which
were broken with #3042

lang/c++/build.sh
@@ -79,7 +79,7 @@ case "$target" in
   lint)
     # some versions of cppcheck seem to require an explicit
     # "--error-exitcode" option to return non-zero code
-    cppcheck --error-exitcode=1 --inline-suppr -f -q -x c++ api examples impl test
+    cppcheck --error-exitcode=1 --inline-suppr -f -q -x c++ include examples impl test
     ;;
 
   test)
@@ -118,7 +118,7 @@ case "$target" in
     ;;
 
   format)
-    clang-format -i --style file `find api -type f` `find impl -type f` `find test -type f`
+    clang-format -i --style file `find include -type f` `find impl -type f` `find test -type f`
     ;;
 
   clean)

lang/c++/impl/DataFile.cc
@@ -22,6 +22,7 @@
 
 #include <sstream>
 
+#include <boost/core/null_deleter.hpp>
 #include <boost/crc.hpp> // for boost::crc_32_type
 #include <boost/iostreams/device/file.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
@@ -63,35 +64,34 @@ boost::iostreams::zlib_params get_zlib_params() {
 }
 } // namespace
 
-DataFileWriterBase::DataFileWriterBase(const char *filename, const ValidSchema &schema, size_t syncInterval,
-                                       Codec codec) : filename_(filename),
-                                                      schema_(schema),
+DataFileWriterBase::DataFileWriterBase(const char *filename,
+                                       const ValidSchema &schema,
+                                       size_t syncInterval,
+                                       Codec codec) : DataFileWriterBase(fileOutputStream(filename), schema, syncInterval, codec) {
+}
+
+DataFileWriterBase::DataFileWriterBase(OutputStream &outputStream,
+                                       const ValidSchema &schema,
+                                       size_t syncInterval,
+                                       Codec codec) : DataFileWriterBase(std::shared_ptr<OutputStream>(&outputStream, boost::null_deleter()), schema, syncInterval, codec) {}
+
+DataFileWriterBase::DataFileWriterBase(std::unique_ptr<OutputStream> outputStream,
+                                       const ValidSchema &schema,
+                                       size_t syncInterval,
+                                       Codec codec) : DataFileWriterBase(std::shared_ptr<OutputStream>(std::move(outputStream)), schema, syncInterval, codec) {}
+
+DataFileWriterBase::DataFileWriterBase(std::shared_ptr<OutputStream> outputStream,
+                                       const ValidSchema &schema,
+                                       size_t syncInterval,
+                                       Codec codec) : schema_(schema),
                                                       encoderPtr_(binaryEncoder()),
                                                       syncInterval_(syncInterval),
                                                       codec_(codec),
-                                                      stream_(fileOutputStream(filename)),
+                                                      stream_(std::move(outputStream)),
                                                       buffer_(memoryOutputStream()),
                                                       sync_(makeSync()),
                                                       objectCount_(0),
                                                       lastSync_(0) {
-    init(schema, syncInterval, codec);
-}
-
-DataFileWriterBase::DataFileWriterBase(std::unique_ptr<OutputStream> outputStream,
-                                       const ValidSchema &schema, size_t syncInterval, Codec codec) : filename_(),
-                                                                                                      schema_(schema),
-                                                                                                      encoderPtr_(binaryEncoder()),
-                                                                                                      syncInterval_(syncInterval),
-                                                                                                      codec_(codec),
-                                                                                                      stream_(std::move(outputStream)),
-                                                                                                      buffer_(memoryOutputStream()),
-                                                                                                      sync_(makeSync()),
-                                                                                                      objectCount_(0),
-                                                                                                      lastSync_(0) {
-    init(schema, syncInterval, codec);
-}
-
-void DataFileWriterBase::init(const ValidSchema &schema, size_t syncInterval, const Codec &codec) {
     if (syncInterval < minSyncInterval || syncInterval > maxSyncInterval) {
         throw Exception(
             "Invalid sync interval: {}. Should be between {} and {}",
@@ -264,8 +264,17 @@ DataFileReaderBase::DataFileReaderBase(const char *filename) : filename_(filenam
     readHeader();
 }
 
-DataFileReaderBase::DataFileReaderBase(std::unique_ptr<InputStream> inputStream) : stream_(std::move(inputStream)),
-                                                                                   decoder_(binaryDecoder()), objectCount_(0), eof_(false), codec_(NULL_CODEC) {
+DataFileReaderBase::DataFileReaderBase(InputStream &inputStream) : DataFileReaderBase(std::shared_ptr<InputStream>(&inputStream, boost::null_deleter())) {
+}
+
+DataFileReaderBase::DataFileReaderBase(std::unique_ptr<InputStream> inputStream) : DataFileReaderBase(std::shared_ptr<InputStream>(std::move(inputStream))) {
+}
+
+DataFileReaderBase::DataFileReaderBase(std::shared_ptr<InputStream> inputStream) : stream_(std::move(inputStream)),
+                                                                                   decoder_(binaryDecoder()),
+                                                                                   objectCount_(0),
+                                                                                   eof_(false),
+                                                                                   codec_(NULL_CODEC) {
     readHeader();
 }
 
@@ -284,8 +293,9 @@ void DataFileReaderBase::init(const ValidSchema &readerSchema) {
 static void drain(InputStream &in) {
     const uint8_t *p = nullptr;
     size_t n = 0;
-    while (in.next(&p, &n))
-        ;
+    while (in.next(&p, &n)) {
+        // do nothing
+    }
 }
 
 char hex(unsigned int x) {

lang/c++/include/avro/DataFile.hh
@@ -59,13 +59,12 @@ typedef std::array<uint8_t, SyncSize> DataFileSync;
  *  this object.
  */
 class AVRO_DECL DataFileWriterBase : boost::noncopyable {
-    const std::string filename_;
     const ValidSchema schema_;
     const EncoderPtr encoderPtr_;
     const size_t syncInterval_;
     Codec codec_;
 
-    std::unique_ptr<OutputStream> stream_;
+    std::shared_ptr<OutputStream> stream_;
     std::unique_ptr<OutputStream> buffer_;
     const DataFileSync sync_;
     int64_t objectCount_;
@@ -85,11 +84,6 @@ class AVRO_DECL DataFileWriterBase : boost::noncopyable {
      * Generates a sync marker in the file.
      */
     void sync();
-
-    /**
-     * Shared constructor portion since we aren't using C++11
-     */
-    void init(const ValidSchema &schema, size_t syncInterval, const Codec &codec);
 
 public:
     /**
@@ -119,7 +113,11 @@ public:
      */
     DataFileWriterBase(const char *filename, const ValidSchema &schema,
                        size_t syncInterval, Codec codec = NULL_CODEC);
+    DataFileWriterBase(OutputStream &outputStream, const ValidSchema &schema,
+                       size_t syncInterval, Codec codec = NULL_CODEC);
     DataFileWriterBase(std::unique_ptr<OutputStream> outputStream,
+                       const ValidSchema &schema, size_t syncInterval, Codec codec);
+    DataFileWriterBase(std::shared_ptr<OutputStream> outputStream,
                        const ValidSchema &schema, size_t syncInterval, Codec codec);
 
     ~DataFileWriterBase();
@@ -154,7 +152,16 @@ public:
     DataFileWriter(const char *filename, const ValidSchema &schema,
                    size_t syncInterval = 16 * 1024, Codec codec = NULL_CODEC) : base_(new DataFileWriterBase(filename, schema, syncInterval, codec)) {}
 
+    /**
+     * Constructs a writer which does not own the outputStream.
+     * The outputStream must remain valid for the DataFileWriter's lifetime
+     */
+    DataFileWriter(OutputStream &outputStream, const ValidSchema &schema,
+                   size_t syncInterval = 16 * 1024, Codec codec = NULL_CODEC) : base_(new DataFileWriterBase(outputStream, schema, syncInterval, codec)) {}
+
     DataFileWriter(std::unique_ptr<OutputStream> outputStream, const ValidSchema &schema,
+                   size_t syncInterval = 16 * 1024, Codec codec = NULL_CODEC) : base_(new DataFileWriterBase(std::move(outputStream), schema, syncInterval, codec)) {}
+    DataFileWriter(std::shared_ptr<OutputStream> outputStream, const ValidSchema &schema,
                    size_t syncInterval = 16 * 1024, Codec codec = NULL_CODEC) : base_(new DataFileWriterBase(std::move(outputStream), schema, syncInterval, codec)) {}
 
     /**
@@ -193,7 +200,7 @@ public:
  */
 class AVRO_DECL DataFileReaderBase : boost::noncopyable {
     const std::string filename_;
-    const std::unique_ptr<InputStream> stream_;
+    const std::shared_ptr<InputStream> stream_;
     const DecoderPtr decoder_;
     int64_t objectCount_;
     bool eof_;
@@ -236,14 +243,27 @@ public:
     void decr() { --objectCount_; }
 
     /**
-     * Constructs the reader for the given file and the reader is
-     * expected to use the schema that is used with data.
-     * This function should be called exactly once after constructing
-     * the DataFileReaderBase object.
+     * Construct a reader for the given filename, an avro::Exception will be thrown
+     * if the file cannot be opened.
      */
     explicit DataFileReaderBase(const char *filename);
 
+    /**
+     * Construct a reader for the given inputStream without taking ownership of
+     * it. The inputStream must remain valid for this object's lifetime.
+     */
+    explicit DataFileReaderBase(InputStream &inputStream);
+
+    /**
+     * Construct a reader for the given inputStream.
+     */
     explicit DataFileReaderBase(std::unique_ptr<InputStream> inputStream);
+
+    /**
+     * Construct a reader for the given inputStream. The inputStream should
+     * be used by at most one DataFileReader/Writer at a time.
+     */
+    explicit DataFileReaderBase(std::shared_ptr<InputStream> inputStream);
 
     /**
      * Initializes the reader so that the reader and writer schemas
@@ -315,7 +335,19 @@ public:
         base_->init(readerSchema);
     }
 
+    /**
+     * Constructs a reader which does not own the inputStream.
+     * The inputStream must remain valid for the DataFileReader's lifetime
+     */
+    DataFileReader(InputStream &inputStream, const ValidSchema &readerSchema) : base_(new DataFileReaderBase(inputStream)) {
+        base_->init(readerSchema);
+    }
+
     DataFileReader(std::unique_ptr<InputStream> inputStream, const ValidSchema &readerSchema) : base_(new DataFileReaderBase(std::move(inputStream))) {
+        base_->init(readerSchema);
+    }
+
+    DataFileReader(std::shared_ptr<InputStream> inputStream, const ValidSchema &readerSchema) : base_(new DataFileReaderBase(std::move(inputStream))) {
         base_->init(readerSchema);
     }
 
@@ -327,7 +359,15 @@ public:
         base_->init();
     }
 
+    explicit DataFileReader(InputStream &inputStream) : base_(new DataFileReaderBase(inputStream)) {
+        base_->init();
+    }
+
     explicit DataFileReader(std::unique_ptr<InputStream> inputStream) : base_(new DataFileReaderBase(std::move(inputStream))) {
+        base_->init();
+    }
+
+    explicit DataFileReader(std::shared_ptr<InputStream> inputStream) : base_(new DataFileReaderBase(std::move(inputStream))) {
         base_->init();
     }
 
@@ -375,40 +415,54 @@ public:
     /**
      * Returns the schema for this object.
      */
-    const ValidSchema &readerSchema() { return base_->readerSchema(); }
+    const ValidSchema &readerSchema() {
+        return base_->readerSchema();
+    }
 
     /**
      * Returns the schema stored with the data file.
      */
-    const ValidSchema &dataSchema() { return base_->dataSchema(); }
+    const ValidSchema &dataSchema() {
+        return base_->dataSchema();
+    }
 
     /**
      * Closes the reader. No further operation is possible on this reader.
      */
-    void close() { return base_->close(); }
+    void close() {
+        return base_->close();
+    }
 
     /**
      * Move to a specific, known synchronization point, for example one returned
      * from previousSync().
      */
-    void seek(int64_t position) { base_->seek(position); }
+    void seek(int64_t position) {
+        base_->seek(position);
+    }
 
     /**
      * Move to the next synchronization point after a position. To process a
      * range of file entries, call this with the starting position, then check
      * pastSync() with the end point before each call to read().
      */
-    void sync(int64_t position) { base_->sync(position); }
+    void sync(int64_t position) {
+        base_->sync(position);
+    }
 
     /**
      * Return true if past the next synchronization point after a position.
      */
-    bool pastSync(int64_t position) { return base_->pastSync(position); }
+    bool pastSync(int64_t position) {
+        return base_->pastSync(position);
+    }
 
     /**
      * Return the last synchronization point before our current position.
      */
-    int64_t previousSync() { return base_->previousSync(); }
+    int64_t previousSync() {
+        return base_->previousSync();
+    }
 };
 
 } // namespace avro

lang/c++/test/CodecTests.cc
@@ -1337,7 +1337,7 @@ static const TestData3 data3[] = {
      "U0N[c3sS1sS2sS3][]",
      1},
     {
-      R"({"name": "Project", "type": "record", "fields": [
+        R"({"name": "Project", "type": "record", "fields": [
         { "name": "_types", "type": [
             "null",
             { "name": "Record1", "type": "record", "fields": [{ "name": "Record1_field1", "type": "string" }]}
@@ -1345,7 +1345,7 @@ static const TestData3 data3[] = {
         { "name": "field1", "type": { "type": "array", "items": "Record1" } }
       ]})",
         "U0N[c3sS1sS2sS3]",
-      R"({"name": "Project", "type": "record", "fields": [
+        R"({"name": "Project", "type": "record", "fields": [
         { "name": "_types", "type": [
             "null",
             { "name": "Record1", "type": "record", "fields": [{ "name": "Record1_field1", "type": "string" }]},
@@ -1375,7 +1375,7 @@ static const TestData3 data3[] = {
         { "name": "field2", "type": { "type": "array", "items": "Record3" }, "default": [] }
       ]})",
         "U0N[c3sS1sS2sS3][]",
-      R"({"name": "Project", "type": "record", "fields": [
+        R"({"name": "Project", "type": "record", "fields": [
         { "name": "_types", "type": [
             "null",
             { "name": "Record1", "type": "record", "fields": [{ "name": "Record1_field1", "type": "string" }]}

lang/c++/test/DataFileTests.cc
@@ -756,6 +756,32 @@ void testSkipString(avro::Codec codec) {
     }
 }
 
+void testSameStreamReadWriteDataFile() {
+    BOOST_TEST_CHECKPOINT(__func__);
+    auto schema = makeValidSchema(dsch);
+    auto stream = avro::memoryOutputStream();
+
+    {
+        ComplexDouble complex;
+        complex.re = 1.0;
+        complex.im = 2.0;
+
+        // Note: We dereference stream here so the writer does not take ownership
+        avro::DataFileWriter<ComplexDouble> writer(*stream, schema);
+        writer.write(complex);
+    }
+
+    // Again, the input stream does not take ownership of the memory stream
+    auto inputStream = avro::memoryInputStream(*stream);
+    // But the reader does take ownership of the input stream
+    avro::DataFileReader<ComplexDouble> reader(std::move(inputStream));
+    ComplexDouble output;
+
+    BOOST_CHECK(reader.read(output));
+    BOOST_CHECK(output.re == 1.0);
+    BOOST_CHECK(output.im == 2.0);
+}
+
 void testSkipStringNullCodec() {
     BOOST_TEST_CHECKPOINT(__func__);
     testSkipString(avro::NULL_CODEC);
@@ -1125,6 +1151,8 @@ init_unit_test_suite(int, char *[]) {
         ts->add(BOOST_CLASS_TEST_CASE(&DataFileTest::testCleanup, t));
         boost::unit_test::framework::master_test_suite().add(ts);
     }
+
+    boost::unit_test::framework::master_test_suite().add(BOOST_TEST_CASE(&testSameStreamReadWriteDataFile));
 
     boost::unit_test::framework::master_test_suite().add(BOOST_TEST_CASE(&testSkipStringNullCodec));
     boost::unit_test::framework::master_test_suite().add(BOOST_TEST_CASE(&testSkipStringDeflateCodec));