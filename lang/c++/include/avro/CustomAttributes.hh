ab67c9965ed61a8070f729631f4232149524c9e6
Josh Humphries <2035234+jhump@users.noreply.github.com>
2024-12-12 19:05:52 -0500

    allow non-string custom attributes

--- lang/c++/include/avro/CustomAttributes.hh
+++ lang/c++/include/avro/CustomAttributes.hh
@@ -19,11 +19,12 @@
 #ifndef avro_CustomAttributes_hh__
 #define avro_CustomAttributes_hh__
 
-#include "Config.hh"
 #include <iostream>
 #include <map>
 #include <optional>
 #include <string>
+
+#include "Config.hh"
 
 namespace avro {
 
@@ -31,24 +32,49 @@ namespace avro {
 // Each attribute is represented by a unique name and value.
 // User is supposed to create CustomAttributes object and then add it to Schema.
 class AVRO_DECL CustomAttributes {
+
 public:
-    // Retrieves the custom attribute json entity for that attributeName, returns an
-    // null if the attribute doesn't exist.
+    // Retrieves the custom attribute value for the given name as a JSON document.
+    // Returns an empty value if the attribute doesn't exist. Unlike getAttribute,
+    // string values will be quoted and escaped.
+    std::optional<std::string> getAttributeJson(const std::string &name) const;
+
+    // Retrieves the custom attribute string for the given name. Returns an empty
+    // value if the attribute doesn't exist. If the attribute value is not a string
+    // (i.e. it's a number, boolean, array, or object), the stringified form (the
+    // value encoded as a JSON document) will be returned.
     std::optional<std::string> getAttribute(const std::string &name) const;
 
-    // Adds a custom attribute. If the attribute already exists, throw an exception.
+    // Adds a custom attribute with an arbitrary JSON value. The given string must
+    // be a valid JSON document. So if the value is a string, it must be quoted
+    // and escaped. Unlike addAttribute, this allows setting the value to
+    // non-string values, like numbers, booleans, arrays, and objects.
+    //
+    // If the attribute already exists or if the given value is not a valid JSON
+    // document, throw an exception.
+    void addAttributeJson(const std::string &name, const std::string &value);
+
+    // Adds a custom attribute with a string value. If the attribute already exists,
+    // throw an exception. Unlike with addAttributeJson, the string value must not
+    // be encoded (no quoting or escaping).
     void addAttribute(const std::string &name, const std::string &value);
 
     // Provides a way to iterate over the custom attributes or check attribute size.
+    // To query for the json element value of an entry, you can iterate over the keys
+    // of this map and then call getAttributeJson for each one.
     const std::map<std::string, std::string> &attributes() const {
-        return attributes_;
+        return attributeStrings_;
     }
 
     // Prints the attribute value for the specific attribute.
     void printJson(std::ostream &os, const std::string &name) const;
 
 private:
-    std::map<std::string, std::string> attributes_;
+    // We have to keep a separate attribute strings map just to implement the
+    // attributes() method. This is just for API backwards-compatibility.
+    std::map<std::string, std::string> attributeStrings_;
+
+    std::map<std::string, std::string> attributeJson_;
 };
 
 } // namespace avro
