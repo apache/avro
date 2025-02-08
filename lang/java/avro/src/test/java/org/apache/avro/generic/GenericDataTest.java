29e72786ba8166db73e8d46fe6115cfe33318a25
Mike Skells <mike.skells@chase.io>
2025-02-08 07:17:14 +0000

    AVRO-4039 fix import that spotless removed

--- lang/java/avro/src/test/java/org/apache/avro/generic/GenericDataTest.java
+++ lang/java/avro/src/test/java/org/apache/avro/generic/GenericDataTest.java
@@ -23,6 +23,13 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.EnumMap;
+import java.util.List;
+import java.util.Map;
+
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
