edb293925d0cd29ae9d0f6031f875722a8912e65
Oscar Westra van Holthe - Kind <opwvhk@users.noreply.github.com>
2025-05-03 21:04:20 +0200

    Avro 4126 fix generated javadoc comment (#3360)

--- lang/java/compiler/src/test/java/org/apache/avro/compiler/specific/TestSpecificCompiler.java
+++ lang/java/compiler/src/test/java/org/apache/avro/compiler/specific/TestSpecificCompiler.java
@@ -133,7 +133,7 @@ public class TestSpecificCompiler {
     boolean compilesWithoutError = cTask.call();
     assertTrue(compilesWithoutError);
     if (!ignoreWarnings) {
-      assertEquals(0, warnings.size(), "Warnings produced when compiling generated code with -Xlint:all");
+      assertEquals(List.of(), warnings, "Warnings produced when compiling generated code with -Xlint:all");
     }
   }
 
