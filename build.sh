a0d0130aea75b8319f251c3805f18a1776efa563
Oscar WvH-K <opwvhk@apache.org>
2025-08-20 01:41:30 +0200

    Fix docs path in build.sh

--- build.sh
+++ build.sh
@@ -183,7 +183,7 @@ do
       cp -r doc/ build/staging-web/
       find build/staging-web/ -type f -print0 | xargs -0 sed -r -i "s#\+\+version\+\+#${VERSION,,}#g"
       mkdir -p build/staging-web/public/docs/
-      mv build/staging-web/doc/content/en/docs/++version++ build/staging-web/public/docs/"${VERSION,,}"
+      mv build/staging-web/content/en/docs/++version++ build/staging-web/public/docs/"${VERSION,,}"
       read -n 1 -s -r -p "Build build/staging-web/ manually now. Press a key to continue..."
       # If it was a SNAPSHOT, it was lowercased during the build.
       cp -R build/staging-web/public/docs/"${VERSION,,}"/* "build/$DOC_DIR/"
@@ -218,7 +218,6 @@ do
         else
           gpg --pinentry-mode loopback --local-user="$GPG_LOCAL_USER" --passphrase "$password" --armor --output "$f.asc" --detach-sig "$f"
         fi
-        
       done
 
       set -x
