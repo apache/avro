756334514492b378042b9e361ba90b5c82426386
Kellen Dye <kellend@spotify.com>
2025-11-06 17:04:32 -0500

    Rv optimize imports

--- lang/java/avro/src/test/java/org/apache/avro/TestSchema.java
+++ lang/java/avro/src/test/java/org/apache/avro/TestSchema.java
@@ -60,7 +60,14 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static java.util.Objects.requireNonNull;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertSame;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class TestSchema {
   @Test
