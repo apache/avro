5525f6148124e18a082c82f649d659454a4af881
H de Vries <hdevries@eventloopsoftware.eu>
2025-12-20 12:38:35 +0100

    Add test for recordSpecificClass

--- lang/java/gradle-plugin/src/test/kotlin/org/apache/avro/gradle/plugin/SchemaCompileTaskTest.kt
+++ lang/java/gradle-plugin/src/test/kotlin/org/apache/avro/gradle/plugin/SchemaCompileTaskTest.kt
@@ -7,6 +7,7 @@ import java.nio.file.Path
 import kotlin.io.path.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertFalse
 import kotlin.test.assertTrue
 
 @ExperimentalPathApi
@@ -74,7 +75,7 @@ class SchemaCompileTaskTest {
 
 
     @Test
-    fun `plugin executes avroGenerateJavaClasses task successfully - for files in test directory`() {
+    fun `plugin executes avroGenerateTestJavaClasses task successfully - for files in test directory`() {
         // given
         val tempSettingsFile = tempDir.resolve("settings.gradle.kts")
         val tempBuildFile = tempDir.resolve("build.gradle.kts")
@@ -196,6 +197,63 @@ class SchemaCompileTaskTest {
 
         val schemaUserContent = testOutPutDirectory.resolve("SchemaUser.java").readText()
         assertTrue(schemaUserContent.contains("It works!"))
+    }
+
+    @Test
+    fun `plugin executes avroGenerateJavaClasses task successfully - custom recordSpecificClass`() {
+        // given
+        val tempSettingsFile = tempDir.resolve("settings.gradle.kts")
+        val tempBuildFile = tempDir.resolve("build.gradle.kts")
+        val tempAvroSrcDir = tempDir.resolve("src/test/avro").createDirectories()
+
+        val testAvroFiles = Path.of("src/test/avro")
+        val testAvroOutPutDir = Path.of("generated-sources/avro")
+
+        val testOutPutDirectory = tempDir.resolve("build/$testAvroOutPutDir/test")
+
+        testAvroFiles.copyToRecursively(
+            tempAvroSrcDir,
+            overwrite = true,
+            followLinks = false
+        )
+
+        tempSettingsFile.writeText("")
+        tempBuildFile.writeText(
+            """            
+            plugins {
+                id("org.apache.avro.avro-gradle-plugin")
+            }
+            
+            avro {
+                schemaType = "schema"
+                sourceDirectory = "$testAvroFiles"
+                outputDirectory = "$testAvroOutPutDir"
+                recordSpecificClass = "org.apache.avro.custom.CustomRecordBase"
+            }
+        """.trimIndent()
+        )
+
+        // when
+        val result = GradleRunner.create()
+            .withProjectDir(tempDir.toFile())
+            .withArguments("avroGenerateJavaClasses")
+            .withPluginClasspath()
+            .forwardOutput() // to see printLn in code
+            .build()
+
+        // then
+        assertEquals(TaskOutcome.SUCCESS, result.task(":avroGenerateJavaClasses")?.outcome)
+
+        val outPutFile = testOutPutDirectory.resolve("SchemaCustom.java")
+        assertTrue(outPutFile.toFile().exists())
+
+        val extendsLines = outPutFile.readLines()
+            .filter { line -> line.contains("class SchemaCustom extends ") }
+        assertEquals(1, extendsLines.size)
+
+        val extendLine = extendsLines[0]
+        assertTrue(extendLine.contains(" org.apache.avro.custom.CustomRecordBase "))
+        assertFalse(extendLine.contains("org.apache.avro.specific.SpecificRecordBase"))
     }
 
 
