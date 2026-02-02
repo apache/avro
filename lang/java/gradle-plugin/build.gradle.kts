2db64b135a4a20e2ca61fbff610edf8eb84bbcef
H de Vries <hdevries@eventloopsoftware.eu>
2026-02-02 16:45:06 +0100

    AVRO-4223
    Format

--- lang/java/gradle-plugin/build.gradle.kts
+++ lang/java/gradle-plugin/build.gradle.kts
@@ -15,6 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 plugins {
   kotlin("jvm") version "2.2.10"
   `java-gradle-plugin`
