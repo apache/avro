c03c4f3f6258472b312e32f1b454c490fb5023fa
Martin Tzvetanov Grigorov <mgrigorov@apache.org>
2026-03-09 13:34:10 +0200

    Trigger the build

--- lang/java/build.sh
+++ lang/java/build.sh
@@ -15,8 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
-set -x
+set -ex
 
 usage() {
   echo "Usage: $0 {lint|test|dist|clean}"
